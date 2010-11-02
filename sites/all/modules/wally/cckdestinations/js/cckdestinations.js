0b49f94963a666b4038435a9e977183fe32bf316
dla <dla@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2010-11-02 09:19:16 +0000

    ajout javascript cascaded select sur edition destinations

---
+++ sites/all/modules/wally/cckdestinations/js/cckdestinations.js
@@ -0,0 +1,35 @@
+function makeSublist(parent,child,isSubselectOptional,childVal)
+{
+	$("body").append("<select style='display:none' id='"+parent+child+"'></select>");
+	$('#'+parent+child).html($("#"+child+" option"));
+	
+		var parentValue = $('#'+parent).attr('value');
+		$('#'+child).html($("#"+parent+child+" .sub_"+parentValue).clone());
+	
+	childVal = (typeof childVal == "undefined")? "" : childVal ;
+	$("#"+child+' option[@value="'+ childVal +'"]').attr('selected','selected');
+	
+	$('#'+parent).change( 
+		function()
+		{
+			var parentValue = $('#'+parent).attr('value');
+			$('#'+child).html($("#"+parent+child+" .sub_"+parentValue).clone());
+			if(isSubselectOptional) $('#'+child).prepend("<option value=''> -- Select -- </option>");
+			$('#'+child).trigger("change");
+                        $('#'+child).focus();
+		}
+	);
+}
+
+$(document).ready(function()
+{
+		$('.tid').each(
+		function(index) {
+		taxo = this.id;
+		targ = taxo.replace("tid", "target");
+		lay = taxo.replace("tid", "layout");
+		makeSublist(targ,lay, true, '');	
+		makeSublist(taxo,targ, false, '1');	
+	});
+  
+});
