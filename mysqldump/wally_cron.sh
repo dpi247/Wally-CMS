9cf91faf557d762268690fe24966a68e245343ed
dla <dla@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2010-11-04 13:06:26 +0000

    add bash command for daily build

---
+++ mysqldump/wally_cron.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+	
+cd /var/www/wally-dev.audaxis.com/
+svn update *
+cd /var/www/wally-dev.audaxis.com/mysqldump/
+mysql -u wally-dev -pWaVhfryR < mybackup.sql
+
+# DLA - Exxodus 
+# create database backup with - #mysqldump --add-drop-table --all-databases -u wally-dev -pWaVhfryR >mybackup.sql
+
