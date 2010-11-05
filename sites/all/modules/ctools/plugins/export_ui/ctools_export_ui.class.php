527f6026a1e0b305c387836a4930440c67d2634f
ddu <ddu@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2010-11-05 16:53:45 +0000

    adding wallyctools module

--- sites/all/modules/ctools/plugins/export_ui/ctools_export_ui.class.php
+++ sites/all/modules/ctools/plugins/export_ui/ctools_export_ui.class.php
@@ -67,13 +67,7 @@ class ctools_export_ui {
    * probably call parent::hook_menu($items) and then modify as needed.
    */
   function hook_menu(&$items) {
-    // During upgrades, the schema can be empty as this is called prior to
-    // actual update functions being run. Ensure that we can cope with this
-    // situation.
-    if (empty($this->plugin['schema'])) {
-      return;
-    }
-
+    
     $prefix = ctools_export_ui_plugin_base_path($this->plugin);
 
     if (isset($this->plugin['menu']['items']) && is_array($this->plugin['menu']['items'])) {
@@ -1089,6 +1083,7 @@ class ctools_export_ui {
    * overridden.
    */
   function edit_form_submit(&$form, &$form_state) {
+    dsm($form);
     if (!empty($this->plugin['form']['submit'])) {
       // Pass $form by reference.
       $this->plugin['form']['submit']($form, $form_state);
