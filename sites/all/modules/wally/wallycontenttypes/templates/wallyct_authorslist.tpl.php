d7cf7b3a3766b09c760a272dbce86b66a22b7b73
dla <dla@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2010-11-18 15:13:29 +0000

    commit about theming cascade / suggestion scheme
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@46615 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

---
+++ sites/all/modules/wally/wallycontenttypes/templates/wallyct_authorslist.tpl.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * $node: $Node to process
+ * $
+ */
+//dsm($node->field_authors_nodes);
+
+?>
+<hr/>
+<div>
+azeazeazeaz <br/>
+azeazeazeaz <br/>
+azeazeazeaz <br/>
+azeazeazeaz <br/>
+azeazeazeaz <br/>
+</div>
+<hr/>
+
