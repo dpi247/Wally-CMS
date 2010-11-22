c60b6db62009d6d34850a2e6f936a378d1d588f4
dla <dla@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2010-11-22 09:18:52 +0000

    misse a jour de panels + correction bug http://rossel.audaxis.com/redmine/issues/849

--- sites/all/modules/wally/wallycontenttypes/templates/wallyct_authorslist.tpl.php
+++ sites/all/modules/wally/wallycontenttypes/templates/wallyct_authorslist.tpl.php
@@ -3,23 +3,25 @@
 /**
  * Wally default template for rendering Packages Autors List 
  */
-function wally_tpl_authors_list($authors) {
+if (!function_exists('wally_tpl_authors_list')) {
+  function wally_tpl_authors_list($authors) {
+    $content = "";
+    foreach ($authors as $author) {
 
-  $content = "";
-  foreach ($authors as $author) {
+      if (isset($author->field_persontaxonomy)) {
+        $content .= "<a href='/taxonomy/term/".$author->field_persontaxonomy[0]['value']."'>".$author->title."</a>";
+      } else {
+        $content .= $author->title;
+      }
 
-    if (isset($author->field_persontaxonomy)) {
-      $content .= "<a href='/taxonomy/term/".$author->field_persontaxonomy[0]['value']."'>".$author->title."</a>";
-    } else {
-      $content .= $author->title;
+      if (isset($author->field_personwebsite)) {
+        $content .= " (<a href='".$author->field_personwebsite[0]["url"]."'>".$author->field_personwebsite[0]["title"]."</a>)";
+        } 
     }
-
-    if (isset($author->field_personwebsite)) {
-      $content .= " (<a href='".$author->field_personwebsite[0]["url"]."'>".$author->field_personwebsite[0]["title"]."</a>)";
-      } 
+    return "<span class='thmr_call'>".$content."</span>"; 
   }
-  return "<span class='thmr_call'>".$content."</span>"; 
-}
+}  
+
 ?>
 <span class="thmr_call">
 <?php print wally_tpl_authors_list($authors); ?>
--- sites/all/modules/wally/wallycontenttypes/templates/wallyct_mainstory.tpl.php
+++ sites/all/modules/wally/wallycontenttypes/templates/wallyct_mainstory.tpl.php
@@ -2,43 +2,47 @@
 /**
  * Wally default template for rendering Packages Autors List 
  * 
+ *
  */
-function wally_tpl_mainstory($story, $node) {
-
-  $content = "";
-  
-  // Barette
-  $textarette = "<span class='barette'>".$story->field_textarette[0]["value"]."</span>";
-
-  // Foretitle
-  $foretitle = "<h3 class='subtitle'>".$textarette." ".$story->field_textforetitle[0]["value"]."</h3>";
-
-  // Title
-  $title = "<h2 class='title'>".$story->title."</h2>";
-
-  // Subtitle
-  $subtitle = "<h3 class='subtitle'>".$story->field_textsubtitle[0]["value"]."</h3>"; 
-
-  // Body
-  $body = "<span class='body'>".$story->field_textbody[0]["value"]."</span>"; 
-
-
  
-  // Authors
-  //$authors = theme("wallyct_authorslist",$story->field_authors, $node);
+if (!function_exists('wally_tpl_mainstory')) {
+  function wally_tpl_mainstory($story, $node) {
 
-  // copyright
-  $copyright = "<span class='copyright'>".$story->field_copyright[0]["value"]."</span>";
+    $content = "";
     
-  $content .= $foretitle; 
-  $content .= $title; 
-  $content .= $subtitle; 
-  $content .= $body; 
-  
-  return $content; 
+    // Barette
+    $textarette = "<span class='barette'>".$story->field_textarette[0]["value"]."</span>";
 
+    // Foretitle
+    $foretitle = "<h3 class='subtitle'>".$textarette." ".$story->field_textforetitle[0]["value"]."</h3>";
+
+    // Title
+    $title = "<h2 class='title'>".$story->title."</h2>";
+
+    // Subtitle
+    $subtitle = "<h3 class='subtitle'>".$story->field_textsubtitle[0]["value"]."</h3>"; 
+
+    // Body
+    $body = "<span class='body'>".$story->field_textbody[0]["value"]."</span>"; 
+      
+    // Authors
+    $authors = theme("wallyct_authorslist",$story->field_authors_nodes, $node);
+
+    // copyright
+    $copyright = "<span class='copyright'>".$story->field_copyright[0]["value"]."</span>";
+      
+    $content .= $foretitle; 
+    $content .= $title; 
+    $content .= $subtitle; 
+    $content .= $body; 
+    $content .= $authors; 
+    
+    return $content; 
+
+  }
 }
-dsm($mainstory); 
+
+//dsm($mainstory); 
 ?>
 
 <div id="mainstory">
