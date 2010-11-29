2292c129b17d61549faad2c2c16342ff91e3a0f6
dla <dla@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2010-11-29 16:24:20 +0000

    selecteur multiple pour les destinations
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@47045 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- sites/all/modules/wally/cckdestinations/js/cckdestinations.js
+++ sites/all/modules/wally/cckdestinations/js/cckdestinations.js
@@ -1,13 +1,8 @@
 function makeSublist(parent,child,isSubselectOptional,childVal)
 {
-  
-  
 	$("body").append("<select style='display:none' id='"+parent+child+"'></select>");
 	$('#'+parent+child).html($("#"+child+" option"));
-	  
-//	var parentValue = $('#'+parent).attr("value");
-
-var parentValue = $('#'+parent).find("option:selected").attr("title");
+  var parentValue = $('#'+parent).find("option:selected").attr("title");
   $('#'+child).html($("#"+parent+child+" .sub_"+parentValue).clone());
 	childVal = (typeof childVal == "undefined")? "" : childVal ;
 	$("#"+child+' option[@value="'+ childVal +'"]').attr('selected','selected');
@@ -27,11 +22,11 @@ $(document).ready(function()
 {
 		$('.tid').each(
 		function(index) {
-		taxo = this.id;
-		targ = taxo.replace("tid", "target");
-		lay = taxo.replace("tid", "layout");
-		makeSublist(targ,lay, true, '');	
-		makeSublist(taxo,targ, true, '');	
+      taxo = this.id;
+      targ = taxo.replace("tid", "target");
+      lay = taxo.replace("tid", "layout");
+      makeSublist(targ,lay, true, '');	
+      makeSublist(taxo,targ, true, '');	
 	});
   
 });
