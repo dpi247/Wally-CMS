d069d88365148c9082254194de77a4eb44adfec7
dla <dla@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2010-12-01 18:14:18 +0000

    some debug
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@47143 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- sites/all/themes/soirmag/templates/custom/soirmag_main_menu.tpl.php
+++ sites/all/themes/soirmag/templates/custom/soirmag_main_menu.tpl.php
@@ -1,7 +1,6 @@
 <?php
 
-    $main_menu = soirmag_tree_output(menu_tree_all_data('primary-links'));
-
+if (!function_exists('soirmag_tree_output')) {
 /**
  * Returns a rendered menu tree.
  *
@@ -12,36 +11,33 @@
  *   The complete, rendered administration menu.
  */
 function soirmag_tree_output($tree, $firstpass=1) { 
-  $output = '';
-  $c = 1;
-  
-  foreach ($tree as $data) {
-
-    // if extra class set, add to the menu item.
-    $extra_class = isset($data['link']['localized_options']['extra class']) ? $data['link']['localized_options']['extra class'] : NULL;
+    $output = '';
     
-    // Get correct link for item.
-    $link = soirmag_item_link($data['link']);
+    foreach ($tree as $data) {
+      // if extra class set, add to the menu item.
+      $extra_class = isset($data['link']['localized_options']['extra class']) ? $data['link']['localized_options']['extra class'] : NULL;
+      // Get correct link for item.
+      $link = soirmag_item_link($data['link']);
+      // is there any sub-menu? 
+      if ($data['below']) {
+        $sub_menu = "<div class='ss-menu'><div>".soirmag_tree_output($data['below'], $firstpass++)."</div></div>";
+        $output .= theme_soirmag_item($link, $data['link']['has_children'], $sub_menu, $data['link']['in_active_trail'], $extra_class);
+      }
+      else {
+        $output .= theme_soirmag_item($link, $data['link']['has_children'], '', $data['link']['in_active_trail'], $extra_class);
+      }
+    }
     
-    // is there any sub-menu? 
-    if ($data['below']) {
-      $sub_menu = "<div class='ss-menu'><div>".soirmag_tree_output($data['below'], $firstpass++)."</div></div>";
-      $output .= theme_soirmag_item($link, $data['link']['has_children'], $sub_menu, $data['link']['in_active_trail'], $extra_class, $c);
+    // @ Fist pass we add a clearfix class to the list.
+    if ($firstpass==1) {
+      return $output ? "\n<ul class='clearfix'>". $output .'</ul>' : '';
+    } else {
+      return $output ? "\n<ul>". $output .'</ul>' : '';
     }
-    else {
-      $output .= theme_soirmag_item($link, $data['link']['has_children'], '', $data['link']['in_active_trail'], $extra_class, $c);
-    }
-  $c++;
-  }
-  
-  // @ Fist pass we add a clearfix class to the list.
-  if ($firstpass==1) {
-    return $output ? "\n<ul class='clearfix'>". $output .'</ul>' : '';
-  } else {
-    return $output ? "\n<ul>". $output .'</ul>' : '';
   }
 }
 
+if (!function_exists('soirmag_item_link')) {
 /**
  * High-performance implementation of theme_menu_item_link().
  *
@@ -49,11 +45,14 @@ function soirmag_tree_output($tree, $firstpass=1) {
  * the admin menu links rendered.
  */
 function soirmag_item_link($link) {
-  // Omit alias lookups.
-  $link['localized_options']['alias'] = TRUE;
-  return '<a href="'. check_url(url($link['href'], $link['localized_options'])) .'">'. (!empty($link['localized_options']['html']) ? $link['title'] : check_plain($link['title'])) .'</a>';
+    // Omit alias lookups.
+    $link['localized_options']['alias'] = TRUE;
+    return '<a href="'. check_url(url($link['href'], $link['localized_options'])) .'">'. (!empty($link['localized_options']['html']) ? $link['title'] : check_plain($link['title'])) .'</a>';
+  }
 }
 
+
+if (!function_exists('theme_soirmag_item')) {
 /**
  * Generate the HTML output for a single menu item and submenu.
  *
@@ -72,15 +71,19 @@ function soirmag_item_link($link) {
  * @ingroup themeable
  */
 function theme_soirmag_item($link, $has_children, $menu = '', $in_active_trail = FALSE, $extra_class = NULL) {
-  $class = ($menu || $has_children ? 'expandable' : '');
-  if (!empty($extra_class)) {
-    $class .= ' '. $extra_class;
+    $class = ($menu || $has_children ? 'expandable' : '');
+    if (!empty($extra_class)) {
+      $class .= ' '. $extra_class;
+    }
+    if ($in_active_trail) {
+      $class .= ' active-trail';
+    }
+    return '<li'. (!empty($class) ? ' class="'. $class .'"' : '') .'>'. $link . $menu .'</li>';
   }
-  if ($in_active_trail) {
-    $class .= ' active-trail';
-  }
-  return '<li'. (!empty($class) ? ' class="'. $class .'"' : '') .'>'. $link . $menu .'</li>';
 }
+
+
+$main_menu = soirmag_tree_output(menu_tree_all_data('primary-links'));
 
 ?>
 <?php print $main_menu; ?>
