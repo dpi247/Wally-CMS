fd8e80caaf1e86fb21c2657f4afaa942eeb826b9
dla <dla@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2010-12-01 17:08:32 +0000

    menu yeah...
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@47138 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- sites/all/themes/soirmag/template.php
+++ sites/all/themes/soirmag/template.php
@@ -1,29 +1,34 @@
 <?php
 
 /**
- *  Implémentation du Hook_theme(); 
+ *  Implémentation du Hook_theme();  
  */
 function soirmag_theme(&$var) {
  return array(
 
     'soirmag_fil_ariane' => array(
     'arguments' => array("destinations" => NULL, "node" => NULL),
-    'template' => 'templates/soirmag_fil_ariane',
+    'template' => 'templates/custom/soirmag_fil_ariane',
     ),
 
     'soirmag_share' => array(
     'arguments' => array("story" => NULL, "node" => NULL),
-    'template' => 'templates/soirmag_share',
+    'template' => 'templates/custom/soirmag_share',
     ),
     
     'soirmag_header' => array(
     'arguments' => array("node" => NULL),
-    'template' => 'templates/soirmag_header',
+    'template' => 'templates/custom/soirmag_header',
     ),
     
     'soirmag_adds' => array(
     'arguments' => array("node" => NULL, "zone" => NULL),
-    'template' => 'templates/soirmag_adds',
+    'template' => 'templates/custom/soirmag_adds',
+    ),
+
+    'soirmag_main_menu' => array(
+    'arguments' => array("menu" => 'primary-links'),
+    'template' => 'templates/custom/soirmag_main_menu',
     ),
     
     
