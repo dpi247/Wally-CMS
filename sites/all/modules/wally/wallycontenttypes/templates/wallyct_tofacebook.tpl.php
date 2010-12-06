c1074f6af7957b4bb26b95c5e1b74c10b3a4ff9b
dla <dla@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2010-12-06 10:07:18 +0000

    admin theme in place

---
+++ sites/all/modules/wally/wallycontenttypes/templates/wallyct_tofacebook.tpl.php
@@ -0,0 +1,22 @@
+<?php
+    $path = base_path() . path_to_theme();
+    $s_title = $node->title;
+    $s_path =  $_SERVER["SERVER_NAME"].url("node/".$node->nid);
+?>
+<p>Partager cet article sur :</p>
+  <ul>
+      <li><a title="sur Facebook" href="http://www.facebook.com/share.php?u=<?php print $s_path; ?>"><img src="<?php print $path; ?>/mediastore/elements/icons/facebook.gif" alt="Facebook" width="16" height="16" /></a></li>
+			<li><a title="sur Twitter" href="http://twitter.com/home?status=<?php print $s_title; ?> - <?php print $s_path; ?>"><img src="<?php print $path; ?>/mediastore/elements/icons/twitter.gif" alt="Twitter" width="16" height="16" /></a></li>
+			<li><a title="sur Del.icio.us" href="http://del.icio.us/post?url=<?php print $s_path; ?>"><img src="<?php print $path; ?>/mediastore/elements/icons/delicious.gif" alt="Delicious" width="16" height="16" /></a></li>
+			<li><a title="sur Digg" href="http://www.digg.com/submit?phase=2&amp;url=<?php print $s_path; ?>"><img src="<?php print $path; ?>/mediastore/elements/icons/digg.gif" alt="Digg" width="16" height="16" /></a></li>
+			<li><a title="sur StumbleUpon" href="http://www.stumbleupon.com/submit?url=<?php print $s_path; ?>"><img src="<?php print $path; ?>/mediastore/elements/icons/stumble.gif" alt="StumbleUpon" width="16" height="16" /></a></li>
+			<li><a title="sur Tumblr" href="http://www.tumblr.com/share?s=<?php print $s_path; ?>&amp;t=<?php print $s_title; ?>"><img src="<?php print $path; ?>/mediastore/elements/icons/tumblr.gif" alt="Tumblr" width="16" height="16" /></a></li>
+  </ul>
+<p>par mail :</p>
+  <ul>
+    <li><a class="par-mail" title="Envoyer cette page par E-mail" href="javascript:void(0)"><img src="<?php print $path; ?>/mediastore/elements/icons/email.gif" alt="E-mail" width="16" height="16" /></a></li>
+  </ul>
+<p>ou imprimez-le :</p>
+<ul>
+  <li><a href="javascript:print();"><img src="<?php print $path; ?>/mediastore/elements/icons/printer.gif" alt="imprimer" width="10" height="10" /></a></li>
+</ul>
