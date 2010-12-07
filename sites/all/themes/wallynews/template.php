bb10b551fbdd1e226139a474e4c3401825d33a7a
dla <dla@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2010-12-07 12:50:22 +0000

    test superfish

---
+++ sites/all/themes/wallynews/template.php
@@ -0,0 +1,18 @@
+<?php
+// $Id: template.php,v 1.16.2.3 2010/05/11 09:41:22 goba Exp $
+
+
+/**
+ * Returns the rendered local tasks. The default implementation renders
+ * them as tabs. Overridden to split the secondary tasks.
+ *
+ * @ingroup themeable
+ */
+function phptemplate_menu_local_tasks() {
+}
+
+/**
+ * Generates IE CSS links for LTR and RTL languages.
+ */
+function phptemplate_get_ie_styles() {
+}
---
+++ sites/all/themes/wallynews/template_o.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Generates IE CSS links for LTR and RTL languages.
+ */
+function phptemplate_get_ie_styles() {
+  $iecss = ""; 
+  
+  return $iecss;
+}
