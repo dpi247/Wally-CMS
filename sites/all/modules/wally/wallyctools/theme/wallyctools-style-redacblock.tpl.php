046616cb48d84bc845b15b72ea7e8d735a6408f6
ddu <ddu@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2010-12-08 19:33:51 +0000

    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@47441 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

---
+++ sites/all/modules/wally/wallyctools/theme/wallyctools-style-redacblock.tpl.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * Template to display destinations views.
+ * 
+ * @see template_preprocess_wallyctools_style_redacblock.
+ *
+ * $view: The view.
+ * $destination: the destination argument.
+ * $target: The targeted block.
+ * 
+ */
+//dsm('Display: '. $view .', destination: '. $destination .', target: '. $target);
+
+?>
