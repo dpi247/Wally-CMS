2c518c69505d288bbbc633bfb10ab23a0ca221aa
dla <dla@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2010-12-08 15:52:35 +0000

    debug

--- sites/all/modules/wally/wallycontenttypes/templates/wallyct_tofacebook.tpl.php
+++ sites/all/modules/wally/wallycontenttypes/templates/wallyct_tofacebook.tpl.php
@@ -3,20 +3,16 @@
     $s_title = $node->title;
     $s_path =  $_SERVER["SERVER_NAME"].url("node/".$node->nid);
 ?>
-<p>Partager cet article sur :</p>
-  <ul>
-      <li><a title="sur Facebook" href="http://www.facebook.com/share.php?u=<?php print $s_path; ?>"><img src="<?php print $path; ?>/mediastore/elements/icons/facebook.gif" alt="Facebook" width="16" height="16" /></a></li>
-			<li><a title="sur Twitter" href="http://twitter.com/home?status=<?php print $s_title; ?> - <?php print $s_path; ?>"><img src="<?php print $path; ?>/mediastore/elements/icons/twitter.gif" alt="Twitter" width="16" height="16" /></a></li>
-			<li><a title="sur Del.icio.us" href="http://del.icio.us/post?url=<?php print $s_path; ?>"><img src="<?php print $path; ?>/mediastore/elements/icons/delicious.gif" alt="Delicious" width="16" height="16" /></a></li>
-			<li><a title="sur Digg" href="http://www.digg.com/submit?phase=2&amp;url=<?php print $s_path; ?>"><img src="<?php print $path; ?>/mediastore/elements/icons/digg.gif" alt="Digg" width="16" height="16" /></a></li>
-			<li><a title="sur StumbleUpon" href="http://www.stumbleupon.com/submit?url=<?php print $s_path; ?>"><img src="<?php print $path; ?>/mediastore/elements/icons/stumble.gif" alt="StumbleUpon" width="16" height="16" /></a></li>
-			<li><a title="sur Tumblr" href="http://www.tumblr.com/share?s=<?php print $s_path; ?>&amp;t=<?php print $s_title; ?>"><img src="<?php print $path; ?>/mediastore/elements/icons/tumblr.gif" alt="Tumblr" width="16" height="16" /></a></li>
-  </ul>
-<p>par mail :</p>
-  <ul>
-    <li><a class="par-mail" title="Envoyer cette page par E-mail" href="javascript:void(0)"><img src="<?php print $path; ?>/mediastore/elements/icons/email.gif" alt="E-mail" width="16" height="16" /></a></li>
-  </ul>
-<p>ou imprimez-le :</p>
-<ul>
-  <li><a href="javascript:print();"><img src="<?php print $path; ?>/mediastore/elements/icons/printer.gif" alt="imprimer" width="10" height="10" /></a></li>
-</ul>
+
+<div class="socialicons">
+  <a href="http://del.icio.us/post?url=<?php print $s_path; ?>&amp;title=<?php print $s_title; ?>" title="Share this post on Delicious"><img src="<?php print $path; ?>/images/social/delicious.png" alt="Share this post on Delicious"></a>
+  <a href="http://www.stumbleupon.com/submit?url=<?php print $s_path; ?>&amp;title=<?php print $s_title; ?>" title="StumbleUpon this post"><img src="<?php print $path; ?>/images/social/stumble.png" alt="StumbleUpon this post"></a>
+  <a href="http://digg.com/submit?phase=2?url=<?php print $s_path; ?>&amp;title=<?php print $s_title; ?>" title="Share this post on Digg"><img src="<?php print $path; ?>/images/social/digg.png" alt="Share this post on Digg"></a>
+  <a href="http://twitthis.com/twit?url=<?php print $s_path; ?>" title="Tweet about this post"><img src="<?php print $path; ?>/images/social/twitter.png" alt="Tweet about this post"></a>
+  <a href="http://www.mix.com/submit?page_url=<?php print $s_path; ?>&amp;title=<?php print $s_title; ?>" title="Share this post on Mixx"><img src="<?php print $path; ?>/images/social/mixx.png" alt="Share this post on Mixx"></a>
+  <a href="http://technorati.com/faves?add=<?php print $s_path; ?>" title="Share this post on Technorati"><img src="<?php print $path; ?>/images/social/technorati.png" alt="Share this post on Technorati"></a>
+  <a href="http://www.facebook.com/sharer.php?u=<?php print $s_path; ?>&amp;t=<?php print $s_title; ?>" title="Share this post on Facebook"><img src="<?php print $path; ?>/images/social/facebook.png" alt="Share this post on Facebook"></a>
+  <a href="http://newsvine.com/_tools/seed&amp;save?u=<?php print $s_path; ?>&amp;h=<?php print $s_title; ?>" title="Share this post on ShareThis"><img src="<?php print $path; ?>/images/social/newsvine.png" alt="Share this post on NewsVine"></a>
+  <a href="http://reddit.com/submit?url=<?php print $s_path; ?>&amp;title=<?php print $s_title; ?>" title="Share this post on Reddit"><img src="<?php print $path; ?>/images/social/reddit.png" alt="Share this post on Reddit"></a>
+  <a href="http://linkedin.com/shareArticle?mini=true&amp;url=<?php print $s_path; ?>&amp;title=<?php print $s_title; ?>" title="Share this post on LinkedIn"><img src="<?php print $path; ?>/images/social/linkedin.png" alt="Share this post on LinkedIn"></a>
+</div>
