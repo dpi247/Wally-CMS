403c68c86618ac55401bf8f0853a4bc343fd031d
ddu <ddu@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2010-12-09 10:30:56 +0000

    revert some files n wallycontent_types
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@47452 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- sites/all/modules/wally/wallycontenttypes/templates/wallyct_mainmenu.tpl.php
+++ sites/all/modules/wally/wallycontenttypes/templates/wallyct_mainmenu.tpl.php
@@ -1,101 +1,72 @@
 <?php
-zef
-  drupal_add_css(drupal_get_path('module', 'wallycontenttypes') . '/css/superfish/superfish.css', 'theme', 'header', FALSE, TRUE, FALSE);
-    drupal_add_js(drupal_get_path('module', 'wallycontenttypes') . '/scripts/superfish/hoverIntent.js', 'theme', 'header', FALSE, TRUE, FALSE);
-if (!function_exists('wallyct_mainmenu_tree_output')) {
 /**
- * Returns a rendered menu tree.
- *
- * @param $tree
- *   A data structure representing the tree as returned from menu_tree_data.
- *
- * @return string
- *   The complete, rendered administration menu.
+ * Wally default template for rendering Presons Lists 
+ * <ul><li></li></ul> style. 
  */
-function wallyct_mainmenu_tree_output($tree, $menuid="menu-primary-links", $firstpass=1) {
 
-    drupal_add_css(drupal_get_path('module', 'wallycontenttypes') . '/css/superfish/superfish.css', 'theme');
-    drupal_add_js(drupal_get_path('module', 'wallycontenttypes') . '/scripts/superfish/hoverIntent.js', 'theme');
-    drupal_add_js(drupal_get_path('module', 'wallycontenttypes') . '/scripts/superfish/superfish.js','theme');
+if (!function_exists('wally_tpl_persons_detail')) {
+  function wally_tpl_persons_detail($persons) {
 
-    drupal_add_js("
-        $(document).ready(function() {
-          jQuery('ul.sf-menu').superfish();
-        });
-    ", 'inline');
+    $content = "";
 
-         
-    $output = '';
+    foreach ($persons as $person) {
 
-    foreach ($tree as $data) {
-      // if extra class set, add to the menu item.
-      $extra_class = isset($data['link']['localized_options']['extra class']) ? $data['link']['localized_options']['extra class'] : NULL;
-      // Get correct link for item.
-      $link = wallyct_mainmenu_menu_item_link($data['link']);
-      // is there any sub-menu? 
-      if ($data['below']) {
-        $sub_menu = wallyct_mainmenu_tree_output($data['below'], "", $firstpass+1);
-        $output .= theme_wallyct_mainmenu_item($link, $data['link']['has_children'], $sub_menu, $data['link']['in_active_trail'], $extra_class);
-      }
-      else {
-        $output .= theme_wallyct_mainmenu_item($link, $data['link']['has_children'], '', $data['link']['in_active_trail'], $extra_class);
-      }
+        $i = 0;
+
+        $t = taxonomy_get_term($person->field_persontaxonomy[0]['value']);
+        $taxo_path = taxonomy_term_path($t);
+
+        if (isset($person->field_persontaxonomy)) {
+          $title = "<a href='".base_path().$taxo_path."'>".$person->title."</a>";
+        } else {
+          $title = $person->title;
+        }
+
+        $content .= "<table>";
+        $content .= "<thead><tr><th colspan='3'>".$title."</th></tr></thead>";
+        $content .= "<tbody>";
+
+        if (isset($person->field_main_picture)) {
+            $content .= "<tr class='".(($i++%2) ? "even" : "odd")."'><td><img src='/".$person->field_main_picture[0]["filepath"]."' width='50' align=tle'left'></td><td>".$person->field_personfirstname[0]["value"]."</td><td>".$person->field_personlastname[0]["value"]."</td></tr>";
+        } else {
+            $content .= "<tr class='".(($i++%2) ? "even" : "odd")."'><td><img src='/".path_to_theme()."/images/default_person_pic.jpg' width='50' align='left'></td><td>".$person->field_personfirstname[0]["value"]."</td><td>".$person->field_personlastname[0]["value"]."</td></tr>";
+        }
+
+        if (isset($person->field_personnickname[0]["value"])) {
+          $content .= "<tr class='".(($i++%2) ? "even" : "odd")."'><td colspan='2' style='align:left'>Nickname:</td><td>".$person->field_personnickname[0]["value"]."</td></tr>";
+        }
+
+        if (isset($person->field_personbirthdate[0]["value"])) {
+          $t = date('jS F Y',strtotime($person->field_personbirthdate[0]["value"]));
+          $content .= "<tr class='".(($i++%2) ? "even" : "odd")."'><td colspan='2' style='align:left'>Birth date:</td><td>".$t."</td></tr>";
+        }
+
+        if (isset($person->field_personsex[0]["value"])) {
+          $content .= "<tr class='".(($i++%2) ? "even" : "odd")."'><td colspan='2' style='align:left'>Genre:</td><td>".$person->field_personsex[0]["value"]."</td></tr>";
+        }
+
+        if (isset($person->field_personemail[0]["value"])) {
+          $content .= "<tr class='".(($i++%2) ? "even" : "odd")."'><td colspan='2' style='align:left'>Email:</td><td>".$person->field_personemail[0]["value"]."</td></tr>";
+        }
+
+        if (isset($person->field_personwebsite[0]["url"])) {
+          if (isset($person->field_personwebsite[0]["title"])) {
+            $t = "<a href='".$person->field_personwebsite[0]["url"]."'>".$person->field_personwebsite[0]["title"]."</a>";
+          } else {
+            $t = "<a href='".$person->field_personwebsite[0]["url"]."'>".$person->field_personwebsite[0]["url"]."</a>";
+          }
+          $content .= "<tr class='".(($i++%2) ? "even" : "odd")."'><td colspan='2' style='align:left'>Website:</td><td>".$t."</td></tr>";
+        }
+
+        if (isset($person->field_phonenumber[0]["value"])) {
+          $content .= "<tr class='".(($i++%2) ? "even" : "odd")."'><td colspan='2' style='align:left'>Phone:</td><td>".$person->field_phonenumber[0]["value"]."</td></tr>";
+        }
+
+        $content .= "</tbody></table>";
     }
     
-    if ($firstpass==1) {
-      return $output ? "\n<ul class='sf-menu' id='".$menuid."'>". $output ."</ul>\n" : "";
-    } else {
-      return $output ? "\n<ul>". $output ."</ul>\n" : "";
-    }
+    return $content; 
   }
 }
-
-
-if (!function_exists('wallyct_mainmenu_menu_item_link')) {
-/**
- * High-performance implementation of theme_menu_item_link().
- *
- * This saves us a theme() call and does only the absolute minimum to get
- * the menu "links" rendered.
- */
-function wallyct_mainmenu_menu_item_link($link) {
-    // Omit alias lookups.
-    $link['localized_options']['alias'] = TRUE;
-    return '<a href="'. check_url(url($link['href'], $link['localized_options'])) .'">'. (!empty($link['localized_options']['html']) ? $link['title'] : check_plain($link['title'])) .'</a>';
-  }
-}
-
-
-if (!function_exists('theme_wallyct_mainmenu_item')) {
-/**
- * Generate the HTML output for a single menu item and submenu.
- *
- * @param string $link
- *   A rendered menu item link.
- * @param bool $has_children
- *   Whether this item has children.
- * @param string $menu
- *   A string containing any rendered children of this item.
- * @param bool $in_active_trail
- *   Whether this item is in the active menu trail.
- * @param string $extra_class
- *   An additional CSS class to set for this item.
- *
- * @see theme_menu_item()
- * @ingroup themeable
- */
-function theme_wallyct_mainmenu_item($link, $has_children, $menu = '', $in_active_trail = FALSE, $extra_class = NULL) {
- //   $class = ($menu || $has_children ? 'expandable' : '');
- //   if (!empty($extra_class)) {
- //     $class .= ' '. $extra_class;
- //   }
- //   if ($in_active_trail) {
- //     $class .= ' active-trail';
- //   }
-    return '<li'. (!empty($class) ? ' class="'. $class .'"' : '') .'>'. $link . $menu .'</li>';
-  }
-}
-
-$main_menu = wallyct_mainmenu_tree_output(menu_tree_all_data($menu), $menuid, 1);
+print wally_tpl_persons_detail($persons);
 ?>
-<?php print $main_menu; ?>
