f22e3b64e4bfb382a91e26e0bf20304cca9b31db
ddu <ddu@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2010-12-09 14:37:52 +0000

    removing themes

--- sites/all/themes/soirmag/templates/node_wally_articlepackage_article.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php
-  drupal_add_css(drupal_get_path('theme', 'soirmag') . '/css/article.css');
-?> 
-<?php
-    if (isset($node->field_mainstory_nodes)) {
-     print theme("wallyct_mainstory", $node->field_mainstory_nodes[0], $node); 
-    }
-?>
