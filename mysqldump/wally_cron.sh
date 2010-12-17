09ead2b1b9edba6690384bd55711182d95138e7d
dla <dla@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2010-12-17 11:18:50 +0000

    default theming options for view in redactionel balcs
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@47849 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- mysqldump/wally_cron.sh
+++ mysqldump/wally_cron.sh
@@ -6,5 +6,5 @@ cd /var/www/wally-dev.audaxis.com/mysqldump/
 mysql -u wally-dev -pWaVhfryR < mybackup.sql
 
 # DLA - Exxodus 
-# create database backup with - #mysqldump --add-drop-table --all-databases -u wally-demo -pwally-demo >mybackup.sql
+# create database backup with - #mysqldump --add-drop-table --all-databases -u wally-dev -pwally-dev >mybackup.sql
 
