384995ad3c07e382f6227811608703465ed53fa4
dla <dla@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2010-12-20 14:47:16 +0000

    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@48021 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- sites/all/themes/wallynews/templates/node-view-default_destination_view-redacblock_1-medium.tpl.php
+++ sites/all/themes/wallynews/templates/node-view-default_destination_view-redacblock_1-medium.tpl.php
@@ -3,16 +3,12 @@
 	$node_path = drupal_get_path_alias("/node/".$node->nid);
 	$mainstory = $node->field_mainstory_nodes[0];
 	$story_path = drupal_get_path_alias("/node/".$mainstory->nid);
-	$mainDest = array();
-	foreach ($node->field_destinations as $destination) {
-		$tempDest['taxID'] = $destination['tid'];
-		$tempDest['tax'] = taxonomy_get_term($destination['tid']);
-		$tempDest['dest'] = $tempDest['tax']->description;
-		$tempDest['path'] = drupal_get_path_alias("/taxonomy/term/".$destination['tid']);
-		$mainDest[] = $tempDest;
-	}
-	$main_image = $node->fields_embededobjects_nodes[0];
-	$teaser_length = 300;
+  $destination_term = theme("wallyct_destinationlist", $node->field_destinations, " | " , "", "");
+
+
+  $main_image = $node->fields_embededobjects_nodes[0];
+	
+  $teaser_length = 300;
 	$teaser = theme("wallyct_teaser", $mainstory->field_textbody[0]['value'], $teaser_length, $node);
   
 	foreach ($node->field_embededobjects_nodes as $n) {
@@ -30,17 +26,7 @@
 </h2>
 <div class="date">
 	Publié le <?php print date('d M Y', $mainstory->created) ?>
-	<span> // 
-		<?php
-			foreach($mainDest as $dest) {
-		?>
-			<a href="<?php print $dest['path'] ?>" title="<?php print $dest['taxID']; ?>" rel="main destination">
-				<?php print $dest['dest']; ?>
-			</a>
-		<?php
-			}
-		?>
-	</span>
+	<span> // <?php print $destination_term; ?></span>
 </div> 
 <a href="<?php print $node_path; ?>" rel="main story title" title="<?php print $mainstory->title; ?>">
 	<?php print $file_img; ?>
