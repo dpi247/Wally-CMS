03267a9b3e8cd1fa7112fb8143e8beeeb8961c0c
dla <dla@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2010-12-22 14:57:48 +0000

    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@48188 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- mysqldump/wally_cron.sh
+++ mysqldump/wally_cron.sh
@@ -3,7 +3,7 @@
 cd /var/www/wally-dev.audaxis.com/
 svn update
 cd /var/www/wally-dev.audaxis.com/mysqldump/
-# mysql -u wally-dev -pWaVhfryR < mybackup.sql
+mysql -u wally-dev -pWaVhfryR < mybackup.sql
 
 # DLA - Exxodus 
 # create database backup with - #mysqldump --add-drop-table --all-databases -u wally-dev -pwally-dev >mybackup.sql
