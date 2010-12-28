50daba19e61842354f73a8296d51be433536efbb
dla <dla@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2010-12-28 11:14:09 +0000

    debu in templates files for main story

--- sites/all/modules/custom/wally/wallycontenttypes/theme/wallyct_mainstory.tpl.php
+++ sites/all/modules/custom/wally/wallycontenttypes/theme/wallyct_mainstory.tpl.php
@@ -4,32 +4,50 @@
  */
      
     // Barette
-    $textbarette = "<span class='barette'>".$mainstory->field_textbarette[0]["value"]."</span>";
+    if ($mainstory->field_textbarette[0]["value"]) {
+      $textbarette = "<span class='barette'>".$mainstory->field_textbarette[0]["value"]."</span>";
+    } else $textbarette = ""; 
     
     // Foretitle
-    $foretitle = $textbarette." ".$mainstory->field_textforetitle[0]["value"];
+    if ($mainstory->field_textforetitle[0]["value"]) {
+      if ($textbarette) {
+        $foretitle = $textbarette." ".$mainstory->field_textforetitle[0]["value"];
+      } else {
+        $foretitle = $mainstory->field_textforetitle[0]["value"];
+      }
+    } else $foretitle = ""; 
 
     // Title
     $title = $mainstory->title;
 
     // Subtitle
-    $subtitle = $mainstory->field_textsubtitle[0]["value"];
+    if ($mainstory->field_textsubtitle[0]["value"]) {
+      $subtitle = $mainstory->field_textsubtitle[0]["value"];
+    } else $subtitle = ""; 
     
     // Body
     $body = $mainstory->field_textbody[0]["value"];
       
     // Authors
-    $authors = "<fieldset id='mainstoryauthors'><h3>Authors</h3>".theme("wallyct_personslist",$mainstory->field_authors_nodes, $mainstory)."</fieldset>";
+    if ($mainstory->field_authors_nodes) {
+      $authors = "<fieldset id='mainstoryauthors'><h3>Authors</h3>".theme("wallyct_personslist",$mainstory->field_authors_nodes, $mainstory)."</fieldset>";
+    } else $authors = ""; 
 
     // Persons
     //$persons = "<fieldset id='mainstoryauthors'><h3>Persons</h3>".theme("wallyct_personslist",$mainstory->field_persons_nodes, $mainstory)."</fieldset>";
-    $persons = "<fieldset id='mainstoryauthors'><h3>Persons</h3>".theme("wallyct_personslist_detail",$mainstory->field_persons_nodes, $mainstory)."</fieldset>";
+    if ($mainstory->field_persons_nodes) {
+      $persons = "<fieldset id='mainstoryauthors'><h3>Persons</h3>".theme("wallyct_personslist_detail",$mainstory->field_persons_nodes, $mainstory)."</fieldset>";
+    } else $persons = ""; 
 
     // copyright
-    $copyright = "<span class='copyright'>".$mainstory->field_copyright[0]["value"]."</span>";
+    if ($mainstory->field_copyright[0]["value"]) {
+      $copyright = "<span class='copyright'>".$mainstory->field_copyright[0]["value"]."</span>";
+    } else $copyright = ""; 
 
     // Photo Gallery Slide Show
-    $photo_gallery = theme("wallyct_photoobject_slider",$node->field_embededobjects_nodes, $node);
+    if ($node->field_embededobjects_nodes) {
+     $photo_gallery = theme("wallyct_photoobject_slider",$node->field_embededobjects_nodes, $node);
+    } else $photo_gallery =""; 
 
     // social network icon
     $social = theme("wallyct_tofacebook", $node);
@@ -38,21 +56,22 @@
     $destination_term = theme("wallyct_destinationlist", $node->field_destinations, " | " , "", "");
 
     // Linsks lists Blocks
-    $linklists = theme("wallyct_linkedobjects", $node->field_linkedobjects_nodes, $node ); 
+    if ($node->field_linkedobjects_nodes) {
+      $linklists = theme("wallyct_linkedobjects", $node->field_linkedobjects_nodes, $node ); 
+    } else $linklists = "";
   
 ?>
 <h3><?php print $foretitle; ?></h3>             
 <h2><a title="" rel="bookmark" href=""><?php print $title; ?></a></h2>             
-<h3><?php print $subtitle; ?></h3>             
+<h3><?php if($subtitle) print $subtitle; ?></h3>             
              
 <div class="date">Publié le <?php print date('d M Y', $mainstory->created) ?>  // <?php print $destination_term; ?></div>
 
 <div class="content">
   <?php  print $photo_gallery; ?>
   <?php print $body; ?>
+  <?php print $linklists; ?>
 </div>
-<div class="clear" />
+<div class="clear"></div>
 <?php print $social; ?>
-<div class="clear" />
-<?php print $linklists; ?>
-<div class="clear" />
+<div class="clear"></div>
