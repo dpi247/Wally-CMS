d043275c9fbee2c771864e215cf74a3088e549ed
dla <dla@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2010-12-30 15:01:35 +0000

    Try to get back to the past ... 2

--- sites/all/themes/wallynews/templates/node_wally_articlepackage_article.tpl.php
+++ sites/all/themes/wallynews/templates/node_wally_articlepackage_article.tpl.php
@@ -2,13 +2,19 @@
   $mainstory = $node->field_mainstory_nodes[0];
 ?>
 <div class="content">
+//// trying to get my node ....<br/>
 <?php
-	if (isset($node->field_mainstory_nodes)) {
+	if (isset($mainstory)) {
 		print theme("wallyct_mainstory", $mainstory, $node); 
   }
 ?>
 <?php
   print theme("wallyct_linkedobjects", $node->field_linkedobjects_nodes, $node);
 ?>
+//// trying to get my node ....<br/>
+//// trying to get my node ....<br/>
+//// trying to get my node ....<br/>
+//// trying to get my node ....<br/>
+//// trying to get my node ....<br/>
 </div>
 <div class="clear"></div>
