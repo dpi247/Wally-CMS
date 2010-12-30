90b2cd10bc98b4427b0c2bd9c861deae9c393871
dla <dla@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2010-12-30 14:34:05 +0000

    the right template
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@48409 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- sites/all/themes/wallynews/templates/node_wally_articlepackage_article.tpl.php
+++ sites/all/themes/wallynews/templates/node_wally_articlepackage_article.tpl.php
@@ -14,7 +14,21 @@
   $subtitle = $mainstory->field_textsubtitle[0]["value"];
     
 ?>
+<h3><?php print $foretitle; ?></h3>             
+<h2><a title="" rel="bookmark" href=""><?php print $title; ?></a></h2>             
+<h3><?php print $subtitle; ?></h3>             
+             
+<div class="date">Publié le <?php print date('d M Y', $mainstory->created) ?> // <a rel="category tag" title="View all posts in Featured" href="http://bignews.blogohblog.net/category/featured/">Featured</a>, <a rel="category tag" title="View all posts in Technology" href="http://bignews.blogohblog.net/category/technology/">Technology</a></span></div>        
+
+<div class="content">
+
+<!-- <p><a href="http://bignews.blogohblog.net/wp-content/uploads/2010/11/First_Xbox_buyer.jpg"><img width="300" height="200" alt="" src="http://bignews.blogohblog.net/wp-content/uploads/2010/11/First_Xbox_buyer-300x200.jpg" title="First_Xbox_buyer" class="alignleft size-medium wp-image-75"></a>  -->
+
 <?php
+	if (isset($node->field_embededobjects_nodes)) {
+		print theme("wallyct_embededobjects", $node->field_embededobjects_nodes, $node);
+	}
+
 	if (isset($node->field_mainstory_nodes)) {
 		print theme("wallyct_mainstory", $mainstory, $node); 
   }
