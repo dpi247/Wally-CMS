28e068a574e539085572d0cc545173ee22fa3638
dla <dla@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2010-12-30 15:13:49 +0000

    Add a comment line for "default node template"
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@48419 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- sites/all/themes/wallynews/templates/node.tpl.php
+++ sites/all/themes/wallynews/templates/node.tpl.php
@@ -1,6 +1,8 @@
 <?php
 // $Id: node.tpl.php,v 1.5 2007/10/11 09:51:29 goba Exp $
 ?>
+
+<h1>DEFAULT NODE TEMPLATE</h1>
 <div id="node-<?php print $node->nid; ?>" class="node<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?>">
 
 <?php print $picture ?>
