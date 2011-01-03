50ebb878815ac8ce5b51559fc02d0404c1abdaa6
ddu <ddu@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-01-03 16:14:40 +0000

    adding templates for embed objects

---
+++ profiles/wally/modules/custom/wally/wallycontenttypes/theme/wallyct_embededobjects.tpl.php
@@ -0,0 +1,45 @@
+<?php
+drupal_add_js(drupal_get_path('theme', 'wallynews').'/scripts/steph/test-jquery.prettyPhoto.js');
+drupal_add_js(drupal_get_path('theme', 'wallynews').'/steph/rotator.js');
+
+?>
+
+
+<script type="text/javascript" charset="utf-8">
+		$(document).ready(function(){
+			$("a[rel^='prettyPhoto']").prettyPhoto();
+		});
+	</script>
+
+<?php  if (count($embededobjects)): ?>
+  <span class="embedobjectsblock">
+ 
+    <div class="photos">
+    <ul>
+ 
+  <?php foreach ($embededobjects as $embededobject) :?>  
+
+        
+      <li>
+        
+        <?php if($embededobject->type=="wally_videoobject"):?>
+        <?php dsm($embededobject->field_photofile[0]);?>
+          <a title="<?php print $embededobject->title?>" rel="prettyPhoto[pp_gal]" href="<?php print $embededobject->field_video3rdparty[0]['embed']; ?>"> 
+            <img src="<?php print $embededobject->field_video3rdparty[0]['data']['thumbnail']['url'];?>" />
+          
+          </a>
+        <?php endif;?>
+        <?php if($embededobject->type=="wally_photoobject"):?>
+         <a title="<?php print $embededobject->title?>" rel="prettyPhoto[pp_gal]" href="/<?php print $embededobject->field_photofile[0]['filepath']; ?>"> 
+            <img src="/<?php print $embededobject->field_photofile[0]['filepath'];?>" />
+          
+          </a>
+       <?php endif;?>
+        
+   
+      </li>
+  <?php endforeach;?>
+    </ul>
+    </div>
+  </span>
+<?php endif;?>
