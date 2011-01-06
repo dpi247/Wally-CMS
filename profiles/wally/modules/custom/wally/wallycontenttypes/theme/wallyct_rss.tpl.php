bfd6468f1d039836abd73fa74f3fa1813fc84978
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-01-06 13:28:06 +0000

    uniformisation rss mix and rss simple

--- profiles/wally/modules/custom/wally/wallycontenttypes/theme/wallyct_rss.tpl.php
+++ profiles/wally/modules/custom/wally/wallycontenttypes/theme/wallyct_rss.tpl.php
@@ -1,24 +1,23 @@
 <?php
 
-if (!function_exists('rss_feed')) {
-  function rss_feed($feed) {
- 
+if (!function_exists('rssmix_feed')) {
+  function rssmix_feed($feed) {
     $content = "";
     foreach ($feed as $k=>$item) {
-      $content .= "<li><a href='".$item['link']."'>".$item['title']."</a></li>";
+      $content .= "<li><a href='".$item['Package']['ExternalURI']['value']."'>".$item['Package']['MainStory']['Title']['value']."</a></li>";
     }
     return $content; 
   }
 }
 
 if ($options['override_title']) {
-    $title = t($options['override_title_text']);
-  } else {
-    $title = t($options['taxonomyfield']);
-  }
-
+  $title = t($options['override_title_text']);
+} else {
+  $title = t($options['taxonomyfield']);
+}
 ?>
-  <h2><? print $title; ?></h2>
-  <ul>
-  <?php print rss_feed($feed); ?>
-  </ul>
+
+<h2><? print $title; ?></h2>
+<ul>
+<?php print rssmix_feed($feed); ?>
+</ul>
