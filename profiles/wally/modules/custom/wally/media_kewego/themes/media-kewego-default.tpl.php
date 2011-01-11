4a3b75826da413c28410a29db08e9d880efbef1d
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-01-11 10:53:55 +0000

    kewego provider update
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@48856 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- profiles/wally/modules/custom/wally/media_kewego/themes/media-kewego-default.tpl.php
+++ profiles/wally/modules/custom/wally/media_kewego/themes/media-kewego-default.tpl.php
@@ -12,22 +12,13 @@
  *  JW Flash Media Player.
  */
  
-$video_id = $variables['variables']['video_id'];
-$video_id = explode('.', $video_id);
-$video_id = $video_id[0];
-$width = media_kewego_variable_get('width') ? media_kewego_variable_get('width') : EMVIDEO_DEFAULT_VIDEO_WIDTH;
-$height = media_kewego_variable_get('height') ? media_kewego_variable_get('height') : EMVIDEO_DEFAULT_VIDEO_HEIGHT;
-$autostart = media_kewego_variable_get('autostart') == 1 ? 'true' : 'false';
-$fullscreen = media_kewego_variable_get('fullscreen') == 1 ? 'true' : 'false';
-$videoformat = media_kewego_variable_get('videoformat') ? media_kewego_variable_get('videoformat') : 'old';
-$language = media_kewego_variable_get('language') ? media_kewego_variable_get('language') : 'en';
-dsm($variables);
+$options = $variables['variables'];
 ?>
-<div id="media-kewego-default-external-<?php print $video_id; ?>">
-  <object id="<?php print $video_id; ?>" type="application/x-shockwave-flash" data="http://www.kewego.com/swf/p3/epix.swf" width="<?php print $width; ?>" height="<?php print $height; ?>">
-    <param name="flashVars" value="language_code=<?php print $language; ?>&playerKey=037fg546ekd7&skinKey=&sig=<?php print $video_id; ?>&autostart=<?php print $autostart; ?>&videoformat=<?php print $videoformat; ?>" />
+<div id="media-kewego-default-external-<?php print $options['video_id']; ?>">
+  <object id="<?php print $options['video_id']; ?>" type="application/x-shockwave-flash" data="http://www.kewego.com/swf/p3/epix.swf" width="<?php print $options['width']; ?>" height="<?php print $options['height']; ?>">
+    <param name="flashVars" value="language_code=<?php print $language; ?>&playerKey=037fg546ekd7&skinKey=&sig=<?php print $options['video_id']; ?>&autostart=<?php print $options['autoplay']; ?>&videoformat=<?php print $options['videoformat']; ?>" />
     <param name="movie" value="http://www.kewego.com/swf/p3/epix.swf" />
-    <param name="allowFullScreen" value="<?php print $fullscreen; ?>" />
+    <param name="allowFullScreen" value="<?php print $options['fullscreen']; ?>" />
     <param name="allowscriptaccess" value="always" />
   </object>
 </div>
