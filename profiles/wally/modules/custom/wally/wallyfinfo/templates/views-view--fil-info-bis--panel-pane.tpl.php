c8f9c9f2e84bdfd10ede475d992854c597504576
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-01-14 09:02:41 +0000

    wallyfinfo added
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@49025 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

---
+++ profiles/wally/modules/custom/wally/wallyfinfo/templates/views-view--fil-info-bis--panel-pane.tpl.php
@@ -0,0 +1,60 @@
+<?php
+// $Id: views-view--fil-info-bis--panel-pane.tpl.php,v 1.0 2011/01/12 ODM $
+/**
+ * @file views-view--fil-info-bis--panel-pane.tpl.php
+ * Default simple view template to display a list of rows.
+ * 
+ * Variables available:
+ * - $view: The view object
+ * - $field: The field handler object that can process the input
+ * - $row: The raw SQL result that can be used
+ *
+ * @ingroup views_templates
+ */
+drupal_add_js(_wallyfinfo_getjsforfilinfotheme(), 'inline');
+drupal_add_js(drupal_get_path('module', 'wallyfinfo').'/scripts/carousel.js');
+drupal_add_css(drupal_get_path('module', 'wallyfinfo').'/css/packfilinfoscarousel.css');
+$results = $view->result;
+$pager = $view->pager;
+?>
+
+<div class="packfil">
+  <div id="colinfos">
+    <h2>Le fil info</h2>
+
+    <div id="carbel" class="carousel">
+      <ul id="listebelgique" class="hideme">
+        <?php foreach ($results as $result) { ?>
+        <li>
+          <div class="belgique">
+            <a href="<?php print drupal_get_path_alias("/node/".$result->nid); ?>"><small><?php print date('H:m', check_plain($result->node_created)); ?></small><h2><?php print check_plain($result->node_title); ?></h2></a>
+          </div>
+        </li>
+        <?php } ?>
+      </ul>
+    </div>
+    
+    <div>
+    <?php
+    if($view->total_rows > $pager['items_per_page']) { 
+      for($i=0; $i<5; $i++) {
+        if($i==$pager['current_page']) {
+    ?>
+      <span><?php print $i; ?></span>
+    <?php
+        } else {
+    ?>
+      <span><a href="?page=<?php print $i; ?>"><?php print $i; ?></a></span>
+    <?php
+        }
+      }
+    }
+    ?>
+    </div>
+
+    <div id="btn_infos">
+      <img src="<?php print drupal_get_path('module', 'wallyfinfo').'/images/previous.png'; ?>" class="prev"/>
+      <img src="<?php print drupal_get_path('module', 'wallyfinfo').'/images/next.png'; ?>" class="next"/>
+    </div>
+  </div>
+</div>
