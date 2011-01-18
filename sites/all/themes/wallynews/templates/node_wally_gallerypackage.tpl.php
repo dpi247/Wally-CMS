28893c72c4410a1e15a0fce95eb5b4f8229680d4
dla <dla@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-01-18 15:47:27 +0000

    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@49189 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- sites/all/themes/wallynews/templates/node_wally_gallerypackage.tpl.php
+++ sites/all/themes/wallynews/templates/node_wally_gallerypackage.tpl.php
@@ -33,42 +33,17 @@ dsm($node);
        	
        	
        	
-       	$result .= "<a href='".$n->field_photofile[0]["filepath"]."' rel='prettyPhoto[pp_gal]' title='title'>";
+       	$result .= "<li><a href='".$n->field_photofile[0]["filepath"]."' rel='prettyPhoto[pp_gal]' title='title'>";
         $result .= $file_img; 
-        $result .= "</a>";
+        $result .= "</a></li>";
 
         $nbr++;
       }
     }
+    print $result;
       
       ?>
-      <li><a href="sites/all/themes/wallynews/images/steph/danneels.jpg" title="Danneels s'explique"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_danneels.jpg"/></a></li>
-      <li><a href="sites/all/themes/wallynews/images/steph/dewever.jpg" rel="prettyPhoto[pp_gal]" title="De Wever est désolé"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_dewever.jpg"/></a></li>
-      <li><a href="http://www.youtube.com/watch?v=xrTpZIz6RvE" rel="prettyPhoto[pp_gal]" title="Vidéo"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_dewever.jpg" alt="Vidéo" /></a></li>
-      <li><a href="sites/all/themes/wallynews/images/steph/dirupo.jpg" rel="prettyPhoto[pp_gal]" title="Di Rupo a une sale tête"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_dirupo.jpg"/></a></li>
-      <li><a href="sites/all/themes/wallynews/images/steph/ecolo.jpg" rel="prettyPhoto[pp_gal]" title="Ecolo et Groen"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_ecolo.jpg"/></a></li>
-      <li><a href="sites/all/themes/wallynews/images/steph/leterme.jpg" rel="prettyPhoto[pp_gal]" title="Yves Leterme"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_leterme.jpg"/></a></li>
-      <li><a href="sites/all/themes/wallynews/images/steph/letermereve.jpg" rel="prettyPhoto[pp_gal]" title="Yves Leterme a de l'espoir"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_letermereve.jpg"/></a></li>
-      <li><a href="sites/all/themes/wallynews/images/steph/vandelanotte.jpg" rel="prettyPhoto[pp_gal]" title="Van De Lanotte arrive au palais royal"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_vandelanotte.jpg"/></a></li>
-      <li><a href="sites/all/themes/wallynews/images/steph/vanrompuy.jpg" rel="prettyPhoto[pp_gal]" title="Van Rompuy"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_vanrompuy.jpg"/></a></li>
-      <li><a href="sites/all/themes/wallynews/images/steph/danneels.jpg" rel="prettyPhoto[pp_gal]" title="Danneels s'explique"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_danneels.jpg"/></a></li>
-      <li><a href="sites/all/themes/wallynews/images/steph/dewever.jpg" rel="prettyPhoto[pp_gal]" title="De Wever est désolé"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_dewever.jpg"/></a></li>
-      <li><a href="http://www.youtube.com/watch?v=xrTpZIz6RvE" rel="prettyPhoto[pp_gal]" title="Vidéo"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_dewever.jpg" alt="Vidéo" /></a></li>
-      <li><a href="sites/all/themes/wallynews/images/steph/dirupo.jpg" rel="prettyPhoto[pp_gal]" title="Di Rupo a une sale tête"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_dirupo.jpg"/></a></li>
-      <li><a href="sites/all/themes/wallynews/images/steph/ecolo.jpg" rel="prettyPhoto[pp_gal]" title="Ecolo et Groen"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_ecolo.jpg"/></a></li>
-      <li><a href="sites/all/themes/wallynews/images/steph/leterme.jpg" rel="prettyPhoto[pp_gal]" title="Yves Leterme"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_leterme.jpg"/></a></li>
-      <li><a href="sites/all/themes/wallynews/images/steph/letermereve.jpg" rel="prettyPhoto[pp_gal]" title="Yves Leterme a de l'espoir"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_letermereve.jpg"/></a></li>
-      <li><a href="sites/all/themes/wallynews/images/steph/vandelanotte.jpg" rel="prettyPhoto[pp_gal]" title="Van De Lanotte arrive au palais royal"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_vandelanotte.jpg"/></a></li>
-      <li><a href="sites/all/themes/wallynews/images/steph/vanrompuy.jpg" rel="prettyPhoto[pp_gal]" title="Van Rompuy"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_vanrompuy.jpg"/></a></li>
-      <li><a href="sites/all/themes/wallynews/images/steph/danneels.jpg" rel="prettyPhoto[pp_gal]" title="Danneels s'explique"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_danneels.jpg"/></a></li>
-      <li><a href="sites/all/themes/wallynews/images/steph/dewever.jpg" rel="prettyPhoto[pp_gal]" title="De Wever est désolé"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_dewever.jpg"/></a></li>
-      <li><a href="http://www.youtube.com/watch?v=xrTpZIz6RvE" rel="prettyPhoto[pp_gal]" title="Vidéo"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_dewever.jpg" alt="Vidéo" /></a></li>
-      <li><a href="sites/all/themes/wallynews/images/steph/dirupo.jpg" rel="prettyPhoto[pp_gal]" title="Di Rupo a une sale tête"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_dirupo.jpg"/></a></li>
-      <li><a href="sites/all/themes/wallynews/images/steph/ecolo.jpg" rel="prettyPhoto[pp_gal]" title="Ecolo et Groen"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_ecolo.jpg"/></a></li>
-      <li><a href="sites/all/themes/wallynews/images/steph/leterme.jpg" rel="prettyPhoto[pp_gal]" title="Yves Leterme"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_leterme.jpg"/></a></li>
-      <li><a href="sites/all/themes/wallynews/images/steph/letermereve.jpg" rel="prettyPhoto[pp_gal]" title="Yves Leterme a de l'espoir"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_letermereve.jpg"/></a></li>
-      <li><a href="sites/all/themes/wallynews/images/steph/vandelanotte.jpg" rel="prettyPhoto[pp_gal]" title="Van De Lanotte arrive au palais royal"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_vandelanotte.jpg"/></a></li>
-      <li><a href="sites/all/themes/wallynews/images/steph/vanrompuy.jpg" rel="prettyPhoto[pp_gal]" title="Van Rompuy"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_vanrompuy.jpg"/></a></li>
+<!--      <li><a href="sites/all/themes/wallynews/images/steph/vanrompuy.jpg" rel="prettyPhoto[pp_gal]" title="Van Rompuy"><img src="sites/all/themes/wallynews/images/steph/thumbnails/t_vanrompuy.jpg"/></a></li>  -->
       </ul>
       </div>
     
