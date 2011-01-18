07eae6288073b91032bf14717b883feb24d2ce2f
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-01-18 14:43:52 +0000

    debug kewego
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@49159 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- profiles/wally/modules/custom/wally/media_kewego/themes/media-kewego-default.tpl.php
+++ profiles/wally/modules/custom/wally/media_kewego/themes/media-kewego-default.tpl.php
@@ -13,10 +13,19 @@
  */
  
 $options = $variables['variables'];
+$expl_vid_id = explode('/', $options['video_id']);
+foreach ($expl_vid_id as $id_part) {
+  if (strstr($id_part, '.html')) {
+    $expl_id_part = explode('.', $id_part);
+    $video_id = $expl_id_part[0];
+    break;
+  }
+  $video_id = $id_part;
+}
 ?>
-<div id="media-kewego-default-external-<?php print $options['video_id']; ?>">
-  <object id="<?php print $options['video_id']; ?>" type="application/x-shockwave-flash" data="http://www.kewego.com/swf/p3/epix.swf" width="<?php print $options['width']; ?>" height="<?php print $options['height']; ?>">
-    <param name="flashVars" value="language_code=<?php print $language; ?>&playerKey=037fg546ekd7&skinKey=&sig=<?php print $options['video_id']; ?>&autostart=<?php print $options['autoplay']; ?>&videoformat=<?php print $options['videoformat']; ?>" />
+<div id="media-kewego-default-external-<?php print $video_id; ?>">
+  <object id="<?php print $video_id; ?>" type="application/x-shockwave-flash" data="http://www.kewego.com/swf/p3/epix.swf" width="<?php print $options['width']; ?>" height="<?php print $options['height']; ?>">
+    <param name="flashVars" value="language_code=<?php print $language; ?>&playerKey=037fg546ekd7&skinKey=&sig=<?php print $video_id; ?>&autostart=<?php print $options['autoplay']; ?>&videoformat=<?php print $options['videoformat']; ?>" />
     <param name="movie" value="http://www.kewego.com/swf/p3/epix.swf" />
     <param name="allowFullScreen" value="<?php print $options['fullscreen']; ?>" />
     <param name="allowscriptaccess" value="always" />
