77f43357f2bb1f54968e60ea4137d8594d2573bf
dla <dla@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-02-01 08:57:11 +0000

    Drupal get path sans le / initial
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@49616 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- sites/all/themes/wallynews/templates/node-view-default_destination_view-redacblock_1-small.tpl.php
+++ sites/all/themes/wallynews/templates/node-view-default_destination_view-redacblock_1-small.tpl.php
@@ -1,8 +1,9 @@
 <?php
 	
-  $node_path = drupal_get_path_alias("/node/".$node->nid);
+			   
+  $node_path = drupal_get_path_alias("node/".$node->nid);
 	$mainstory = $node->field_mainstory_nodes[0];
-	$story_path = drupal_get_path_alias("/node/".$mainstory->nid);
+	$story_path = drupal_get_path_alias("node/".$mainstory->nid);
   $destination_term = theme("wallyct_destinationlist", $node->field_destinations, " | " , "", "");
   
 ?>
