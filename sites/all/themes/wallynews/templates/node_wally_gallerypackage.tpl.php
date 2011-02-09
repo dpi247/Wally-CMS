d823f709009330bea560db3e44c24421715fc8a6
dla <dla@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-02-09 09:52:13 +0000

    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@49981 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- sites/all/themes/wallynews/templates/node_wally_gallerypackage.tpl.php
+++ sites/all/themes/wallynews/templates/node_wally_gallerypackage.tpl.php
@@ -1,4 +1,9 @@
 <?php
+/*
+ *
+ * 
+ */
+
   $node_path = drupal_get_path_alias("/node/".$node->nid);
   $imgstory = $node->field_embededobjects_nodes;
   $destination_term = theme("wallyct_destinationlist", $node->field_destinations, " | " , "", "");
@@ -7,25 +12,18 @@
   //$main_edition = $field_editions [0]['value'];
   //$main_channel = $field_channels [0]['value'];
   $presetname='gallery_preset';
-foreach ($imgstory as $n) {
-		if ($n->type == "wally_photoobject") {
-      
-      $file_path = $n->field_photofile[0]['filepath'];
-      $explfilepath = explode('/', $file_path );
-	  $file_img[] = theme('imagecache', $presetname,  $file_path);//, array('class'=>'img'));
-	  $path_photo[] = imagecache_create_url($presetname, $file_path); 
-	  $title_img[] = $n->title;
-	  $summary[]= $n->field_summary[0]['value'];
-		
-		
-		}
-		
-	}
 
+  foreach ($imgstory as $n) {
+  	if ($n->type == "wally_photoobject") {
+  		$file_path = $n->field_photofile[0]['filepath'];
+  		$explfilepath = explode('/', $file_path );
+  		$file_img[] = theme('imagecache', $presetname,  $file_path);//, array('class'=>'img'));
+  		$path_photo[] = imagecache_create_url($presetname, $file_path);
+  		$title_img[] = $n->title;
+  		$summary[]= $n->field_summary[0]['value'];
+  	}
+  }
 ?>
-	<?//AFFICHAGE
-	  // Nom de la photo ?>
-
 <div id="gal2" class="gallery">
 <h2><?php print $node->title ; ?></h2>
 <?php print $main_summary ; ?> <br/> 
@@ -34,7 +32,6 @@ foreach ($imgstory as $n) {
     <?php print $file_img[0]; ?>
       <div class="desc">
         <a href="#" class="collapse">Close Me!</a>
-          
       </div>
       <div class="block">
             <h2><?php print $title_img[0]; ?></h2>
@@ -42,10 +39,9 @@ foreach ($imgstory as $n) {
 					 Publié le <?php print date('d M Y', $imgstory[0]->created) ?>
 					 <span> // <?php print $destination_term; ?></span>
 				  </div>
-				<p> <?php print $summary[0] ?></p>
+					<p> <?php print $summary[0] ?></p>
           </div>
   </div>
-
   <div class="image_thumb">
     <ul>
       <?php $i = 0; ?>
