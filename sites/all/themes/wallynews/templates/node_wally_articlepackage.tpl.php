805076aceee26c1fca7510e50d0a27b4b7f38ec0
ddu <ddu@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-02-15 09:48:28 +0000

    Adding default templates
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@50277 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

---
+++ sites/all/themes/wallynews/templates/node_wally_articlepackage.tpl.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * Template for rendering a articlepackage-article node type.
+ */
+
+  $mainstory = $node->field_mainstory_nodes[0];
+?>
+    
+<div class="content">
+<h3>NID: <?php print $node->nid; ?></h3>
+<?php
+	if (isset($mainstory)) {
+		print theme("wallyct_mainstory", $mainstory, $node); 
+  }
+?>
+<?php
+//echo '<pre>'.print_r($node,1)."</pre>"; 
+ print theme("wallyct_embededobjects", $node->field_embededobjects_nodes, $node);
+?>
+
+<?php
+  print theme("wallyct_linkedobjects", $node->field_linkedobjects_nodes, $node);
+?>
+
+
+</div>
+<div class="clear"></div>
