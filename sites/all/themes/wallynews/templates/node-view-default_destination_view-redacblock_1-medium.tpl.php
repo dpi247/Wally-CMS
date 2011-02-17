b2f013d18e0c512e205d04d47f0bdf171c2fc086
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-02-17 15:44:25 +0000

    clean code
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@50502 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- sites/all/themes/wallynews/templates/node-view-default_destination_view-redacblock_1-medium.tpl.php
+++ sites/all/themes/wallynews/templates/node-view-default_destination_view-redacblock_1-medium.tpl.php
@@ -5,9 +5,6 @@
 	$mainstory = $node->field_mainstory_nodes[0];
 	$story_path = drupal_get_path_alias("node/".$mainstory->nid);
   $destination_term = theme("wallyct_destinationlist", $node->field_destinations, " | " , "", "");
-
-
-  $main_image = $node->fields_embededobjects_nodes[0];
 	
   $teaser_length = 300;
 	$teaser = theme("wallyct_teaser", $mainstory->field_textbody[0]['value'], $teaser_length, $node);
