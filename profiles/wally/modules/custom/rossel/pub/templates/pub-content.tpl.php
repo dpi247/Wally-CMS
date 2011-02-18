fa6d25ab2a9eb9a1be6c2d99f21399bc05bace13
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-02-18 13:41:05 +0000

    maj pub

--- profiles/wally/modules/custom/rossel/pub/templates/pub-content.tpl.php
+++ profiles/wally/modules/custom/rossel/pub/templates/pub-content.tpl.php
@@ -2,7 +2,7 @@
 // $Id: pub-content.tpl.php,v 1.0 2011/02/18 ODM $
 
 /**
- * Available variable:
+ * Available variables:
  *  - $html_code : contents the code returned by the webservice
  *  - $delta : the delta of the block
  */
