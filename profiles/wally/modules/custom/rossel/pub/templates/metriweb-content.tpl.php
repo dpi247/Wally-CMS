81b8652d786f7824da849ecd986f2fdebfcf6377
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-02-18 13:06:17 +0000

    maj pub
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@50546 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

---
+++ profiles/wally/modules/custom/rossel/pub/templates/metriweb-content.tpl.php
@@ -0,0 +1,5 @@
+<?php
+// $Id: pub-content.tpl.php,v 1.0 2011/02/18 ODM $
+dsm(func_get_args());
+dsm(debug_backtrace());
+?>
---
+++ profiles/wally/modules/custom/rossel/pub/templates/pub-content.tpl.php
@@ -0,0 +1,5 @@
+<?php
+// $Id: pub-content.tpl.php,v 1.0 2011/02/18 ODM $
+?>
+
+<?php print $html_code; ?>
