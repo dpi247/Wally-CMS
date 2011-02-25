e3c520a66830e475e87d7e88c753e6db8144191f
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-02-25 09:02:51 +0000

    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@50939 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- profiles/wally/modules/custom/wally/wallyfinfo/templates/views-view--fil-info--panel-pane-2--li.tpl.php
+++ profiles/wally/modules/custom/wally/wallyfinfo/templates/views-view--fil-info--panel-pane-2--li.tpl.php
@@ -12,11 +12,12 @@
  */
 $node = node_load($view_result->nid);
 wallycontenttypes_packagepopulate($node, null);
+$text_body = $node->field_mainstory_nodes[0]->field_textbody[0]['value'];
 ?>
 <li>
   <div class="<?php print strtolower($destination->name); ?>_page">
     <h2 class="title"><?php print check_plain($node->title); ?></h2>
     <p class="date"><?php print date('d M Y, H:m', $node->created); ?></p> 
-    <?php print $node->field_mainstory_nodes[0]->field_textbody[0]['value']; ?>
+    <?php print (strlen($text_body) > 250) ? substr($text_body, 0, 250).'...' : $text_body; ?>
   </div>
 </li>
