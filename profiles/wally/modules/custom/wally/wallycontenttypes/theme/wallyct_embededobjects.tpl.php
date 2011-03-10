c45b8eadd33a4babb2885d718749d66bd406d82f
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-03-10 16:06:55 +0000

    theming

--- profiles/wally/modules/custom/wally/wallycontenttypes/theme/wallyct_embededobjects.tpl.php
+++ profiles/wally/modules/custom/wally/wallycontenttypes/theme/wallyct_embededobjects.tpl.php
@@ -1,29 +1,19 @@
 <?php
-drupal_add_js(drupal_get_path('theme', 'wallynews').'/scripts/steph/test-jquery.prettyPhoto.js');
-drupal_add_js(drupal_get_path('theme', 'wallynews').'/steph/rotator.js');
-
+drupal_add_js(drupal_get_path('theme', 'wallynews').'/scripts/rotator.js');
 ?>
 
-
-<script type="text/javascript" charset="utf-8">
-  $(document).ready(function(){
-    $("a[rel^='prettyPhoto']").prettyPhoto();
-  });
-</script>
-
-<?php  if (count($embededobjects)): ?>
+<?php  if (count($embededobjects)>0): ?>
   <span class="embedobjectsblock">
     <div class="photos">
-    <ul>
- 
-  <?php foreach ($embededobjects as $embededobject) :
+      <ul>
+        <?php foreach ($embededobjects as $embededobject) :
           $type = explode('_', $embededobject->type);
-  ?>  
-      <li>    
-        <?php print theme('wallyct_'.$type[1], $embededobject); ?>
-      </li>
-  <?php endforeach;?>
-    </ul>
+        ?>  
+        <li>    
+          <?php print theme('wallyct_'.$type[1], $embededobject); ?>
+        </li>
+        <?php endforeach;?>
+      </ul>
     </div>
   </span>
 <?php endif;?>
