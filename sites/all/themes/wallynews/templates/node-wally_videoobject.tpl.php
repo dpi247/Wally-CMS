e956a05dfe97e6a12ee9e5deb17a6e8cbc223b3a
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-03-11 15:44:02 +0000

    video 3rd party themed
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@52006 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- sites/all/themes/wallynews/templates/node-wally_videoobject.tpl.php
+++ sites/all/themes/wallynews/templates/node-wally_videoobject.tpl.php
@@ -4,16 +4,5 @@
  * 
  */
 
-
 ?>
-<li class="thumb">
-  <a href=" <?php print $path_photo; ?> "><?php print $image; ?></a>
-  <div class="block">
-    <h2><?php print $title_img; ?></h2>
-    <div class="date">
-      Publié le <?php print date('d M Y', $node->created) ?>
-      <span> <?php //print $destination_term; ?></span>
-    </div>
-    <p> <?php print $summary ?></p>
-  </div>
-</li>
+<?php print $node->content['group_video']['group']['field_video3rdparty']['field']['#children']; ?>
