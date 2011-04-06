5aae4d391b73d7daa8ea5586b3c8d2f5e1ba8f91
gja <gja@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-04-06 09:21:53 +0000

    maj preview

---
+++ profiles/wally/modules/custom/wally/wallyedit/theme/wallyedit-preview-iframe.tpl.php
@@ -0,0 +1,9 @@
+<?php
+// $Id
+?>
+
+<br />
+<h1>Preview : </h1>
+<iframe src="/node/<?php print $cache_name?>/preview2" width="100%" height="500">
+  <p>Your browser does not support iframes.</p>
+</iframe>
