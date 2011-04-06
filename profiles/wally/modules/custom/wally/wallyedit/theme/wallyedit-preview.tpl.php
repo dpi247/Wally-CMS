27ed03fa6bda60f09271bee10c6a99be40c414d7
gja <gja@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-04-06 09:33:13 +0000

    maj preview wallyedit
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@53829 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- profiles/wally/modules/custom/wally/wallyedit/theme/wallyedit-preview.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php
-// $Id
-?>
-<h1>Preview : </h1>
-<iframe src="/node/<?php print $cache_name?>/preview2" width="100%" height="500">
-      <p>Your browser does not support iframes.</p>
-</iframe>
