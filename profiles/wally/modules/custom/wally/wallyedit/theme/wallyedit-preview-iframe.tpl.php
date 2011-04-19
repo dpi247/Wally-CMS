e0afdd63c3722b05964fb5daeb651d1acaaff59d
gja <gja@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-04-19 08:28:15 +0000

    preview for all destinations
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@54527 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- profiles/wally/modules/custom/wally/wallyedit/theme/wallyedit-preview-iframe.tpl.php
+++ profiles/wally/modules/custom/wally/wallyedit/theme/wallyedit-preview-iframe.tpl.php
@@ -4,6 +4,6 @@
 
 <br />
 <h1>Preview : </h1>
-<iframe src="/node/<?php print $cache_name?>/preview2" width="100%" height="500">
+<iframe id="prev_iframe" src="/node/<?php print $vars['cache_name']?>/preview2/<?php print $vars['dest']?>" width="100%" height="500">
   <p>Your browser does not support iframes.</p>
 </iframe>
