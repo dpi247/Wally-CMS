9d7e2fad8a39fdebbe31ef92b69aada967fa6ed8
jsl <jsl@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-05-09 09:19:21 +0000

    modif des presets

--- profiles/wally/modules/custom/wally/wallyrsspublish/theme/views-view-row-rss.tpl.php
+++ profiles/wally/modules/custom/wally/wallyrsspublish/theme/views-view-row-rss.tpl.php
@@ -15,7 +15,7 @@
   <?php if (!empty($url)){?>
   <media:content url="<?php print $url;?>" type="<?php print $type;?>" fileSize="<?php print $filesize;?>">
   <media:title><?php print $title;?></media:title>
-  <media:thumbnail url="<?php print $url;?>" height='360' width='480'/>
+  <media:thumbnail url="<?php print $url_thumb;?>"/>
   <media:description type="html"><?php print $description?></media:description>
   </media:content>
   <?php }?>
