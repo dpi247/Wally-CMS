d55a57f210f6fbdcbf17dd0a1a46aad6cbe57633
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-05-17 13:51:11 +0000

    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@56080 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- profiles/wally/modules/mod/simplesamlphp_auth/toolbarJSON.php
+++ profiles/wally/modules/mod/simplesamlphp_auth/toolbarJSON.php
@@ -20,7 +20,7 @@
 		$items = build_menu_items('toolbar_nc.yaml');
 		$jsonData = array('items' => $items, 'mode' => 'unauthenticated', 'iframe' => $infoSSO[1]);
 	}
-	print 'cocu';
+	dsm('coucou');
 
 	echo $_GET['cbk']."(".json_encode($jsonData).")";
 	
