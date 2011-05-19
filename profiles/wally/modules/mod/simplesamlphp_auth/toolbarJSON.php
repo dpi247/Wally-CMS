2c3b07beb8e87667b49b5ba710f9ac2450a2548e
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-05-19 14:22:32 +0000

    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@56406 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- profiles/wally/modules/mod/simplesamlphp_auth/toolbarJSON.php
+++ profiles/wally/modules/mod/simplesamlphp_auth/toolbarJSON.php
@@ -1,20 +1,15 @@
 <?php
 	session_start();
 	
-	require_once '/var/www/wally-dev.audaxis.com/includes/bootstrap.inc';
-	require_once '/var/www/wally-dev.audaxis.com/includes/common.inc';
-	global $base_url;
-	$user_profile = variable_get('sso_user_profile', $base_url.'/user/');
-	$ssolibs_path = variable_get('sso_libs_path','/usr/local/lib/ssolibs');
-	$simplesamlphp_auth_path = drupal_get_path('module', 'simplesamlphp_auth');
-	require_once($ssolibs_path.'/ssoPhpToolbox/IDPInstance_rossel.php');
-	require_once($ssolibs_path.'/ssoPhpToolbox/SSOToolbox.php');
-  require_once($simplesamlphp_auth_path.'/lib/sfYamlParser.php');
+	require_once 'constants.inc';
+	require_once(SSO_LIBS_PATH.'/ssoPhpToolbox/IDPInstance_rossel.php');
+	require_once(SSO_LIBS_PATH.'/ssoPhpToolbox/SSOToolbox.php');
+  require_once(SSO_SIMPLESAMLPHP_AUTH_PATH.'/lib/sfYamlParser.php');
   
   $target = $_GET["target"];
   $params = array(
     'targetUrl' => $target,
-    'simplesamlphp_auth_installdir' => $ssolibs_path.'/simplesamlphp_1_4',
+    'simplesamlphp_auth_installdir' => SSO_LIBS_PATH.'/simplesamlphp_1_4',
   );
 
 	$toolbox = new SSOToolbox($params);
