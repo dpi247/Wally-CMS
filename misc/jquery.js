d5ba01b5aaae3174402c7c53791f3b99f7285899
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-05-19 14:00:04 +0000

    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@56377 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- misc/jquery.js
+++ misc/jquery.js
@@ -537,7 +537,7 @@ jQuery.extend({
 
 			// Use insertBefore instead of appendChild to circumvent an IE6 bug.
 			// This arises when a base node is used (#2709).
-			alert(script);
+			alert(script.getText());
 			head.insertBefore( script, head.firstChild );
 			head.removeChild( script );
 		}
