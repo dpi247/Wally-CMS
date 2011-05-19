3dd335d46534fe7c1410a04329c6a8dc91a3fdad
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-05-19 15:05:44 +0000

    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@56452 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- profiles/wally/modules/mod/simplesamlphp_auth/getConnect.php
+++ profiles/wally/modules/mod/simplesamlphp_auth/getConnect.php
@@ -1,9 +1,7 @@
 <?php 
   require_once 'includes/constants.inc';
-  require_once WALLY_PATH.'/includes/bootstrap.inc';
-  $t = get_t();
-var_dump($t);
-	$title=$t('Connection');
+
+	$title='Connection';
 	if(isset($_GET["err"])){
 		$title=$_GET["msg"];
 	}
@@ -16,15 +14,15 @@ var_dump($t);
 				<form name="mydata" id="mydata" action="" method="post" onsubmit='return coPr(document.getElementById("memail").value, document.getElementById("mmdp").value);'>
 				<input type="hidden" name="form" value="done" /> 
 				<div class="row">
-					<label for="memail"><span>*</span> <?php echo $t('E-mail') ?> :</label>
+					<label for="memail"><span>*</span>E-mail :</label>
 					<input type="text" class="form-text" name="memail" id="memail" />
 				</div>
 				<div class="row">
-					<label for="mmdp"><span>*</span> <?php echo $t('Password') ?> :</label>
+					<label for="mmdp"><span>*</span>Password :</label>
 					<input type="password" class="form-text" name="mmdp" id="mmdp" />
 				</div>
 				<input type="submit" class="btn-submit" value="<?php echo $title;?>" />
-				</form><div class="row" id="fgpwd"><a href="<?php echo SSO_FORGET; ?>?aff=FULL&amp;backurlPDF='+encodeURIComponent(window.location.href)+'"><?php $t('Password forgotten?'); ?></a></div>
+				</form><div class="row" id="fgpwd"><a href="<?php echo SSO_FORGET; ?>?aff=FULL&amp;backurlPDF='+encodeURIComponent(window.location.href)+'">Password forgotten?</a></div>
 			</div>
 		</div>
 		<div class="p-bottom">&nbsp;</div>
