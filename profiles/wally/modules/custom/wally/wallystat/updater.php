496c110bff6079344288b5fc95ae5eb8d4cca1b7
gja <gja@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-05-20 14:03:17 +0000

    wallystat upgrade
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@56578 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- profiles/wally/modules/custom/wally/wallystat/updater.php
+++ profiles/wally/modules/custom/wally/wallystat/updater.php
@@ -18,19 +18,33 @@ if(isset($_GET['nid']) && is_numeric($_GET['nid'])){
 			 $terms = implode(' ',$safe_terms);
 		}
 	}
-	
-	if(isset($terms) && $terms != ''){
-		$query = "INSERT INTO wallystat_tempdata (`nid`, `timestamp`, `terms`) VALUES (".$nid.", ".$tstamp.", '".$terms."')";
-	}else{
-		$query = "INSERT INTO wallystat_tempdata (`nid`, `timestamp`) VALUES (".$nid.", ".$tstamp.")";
-	}
-	$wally_install_root = $_SERVER['DOCUMENT_ROOT'];
+  
+  $wally_install_root = $_SERVER['DOCUMENT_ROOT'];
 	
 	chdir($wally_install_root);
 	include_once ('./includes/bootstrap.inc');
 	drupal_bootstrap(DRUPAL_BOOTSTRAP_DATABASE);
+
+  $params = array();
+  $params[] = $_GET['param01'];
+  $params[] = $_GET['param02'];
+  $params[] = $_GET['param03'];
+  $params[] = $_GET['param04'];
+  $params[] = $_GET['param05'];
+  $params[] = $_GET['param06'];
+  $params[] = $_GET['param07'];
+  $params[] = $_GET['param08'];
+  $params[] = $_GET['param09'];
+  $params[] = $_GET['param10'];
+
+	if(isset($terms) && $terms != ''){
+		db_query($query = "INSERT INTO wallystat_tempdata (`nid`, `title`, `param01`, `param02`, `param03`, `param04`, `param05`, `param06`, `param07`, `param08`, `param09`, `param10`, `timestamp`, `terms`) VALUES (%d, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', %d, '%s')", $nid, $_GET['title'], $_GET['param01'], $_GET['param02'], $_GET['param03'], $_GET['param04'], $_GET['param05'], $_GET['param06'], $_GET['param07'], $_GET['param08'], $_GET['param09'], $_GET['param10'], $tstamp, $terms);//(".$nid.", ".$tstamp.", '".$terms."')";
+	}else{
+		$query = "INSERT INTO wallystat_tempdata (`nid`, `timestamp`) VALUES (".$nid.", ".$tstamp.")";
+	}
+
 	
-	$result = db_query($query);
+	//$result = db_query($query);
 	//var_dump($result);
 	//echo "<hr/>";
 	
