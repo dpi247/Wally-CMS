faf7809f01ce7bcbfbc612359756207cd4babb56
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-05-27 15:22:10 +0000

    stat real-time rendering

---
+++ profiles/wally/modules/custom/wally/wallystat/templates/wallystat-show-stats-param.tpl.php
@@ -0,0 +1,30 @@
+<?php
+
+$href_value = $_GET['hrefvalue'];
+$node_path = drupal_get_normal_path(trim($href_value, '/'));
+$expl_path = explode('/', $node_path);
+
+if ($expl_path[0] == 'node' && is_numeric($expl_path[1])) {
+  $nid = $expl_path[1];
+  $temp_node_hours_stats = array();
+  $param_name_id = ($param_i == 10) ? 'param'.$param_i : 'param0'.$param_i;
+  $node_stats_db = db_query('SELECT %s, SUM(totalcount) FROM {wallystat_node_counter} WHERE nid = "%s" GROUP BY %s', $param_name_id, $nid, $param_name_id);
+
+  while ($stat = db_fetch_array($node_stats_db)) {
+    $node_stats[] = $stat['SUM(totalcount)'];
+    $labels[] = ($stat[$param_name_id] == '') ? 'Not set' : $stat[$param_name_id];
+  }
+  
+  $chart = array(
+    '#chart_id' => 'node_views_'.$param_name_id,
+    '#title' => t($params_callback[$params_callback['current_callback']]['name']),
+    '#type' => CHART_TYPE_PIE,
+    '#size' => chart_size(240, 150),
+    '#data' => $node_stats,
+    '#labels' => $labels,
+  );
+  
+  print chart_render($chart);
+}
+
+?>
