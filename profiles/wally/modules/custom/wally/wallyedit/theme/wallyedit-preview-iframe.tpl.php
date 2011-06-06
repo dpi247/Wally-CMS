dba4b73c8667020d4fd939f6881cc3c840591dc6
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-06-06 14:52:08 +0000

    preview

--- profiles/wally/modules/custom/wally/wallyedit/theme/wallyedit-preview-iframe.tpl.php
+++ profiles/wally/modules/custom/wally/wallyedit/theme/wallyedit-preview-iframe.tpl.php
@@ -3,6 +3,6 @@
 ?>
 
 <h1>Preview (Will be displayed in all the destination views of the page): </h1>
-<iframe id="prev_iframe" src="/node/<?php print $vars['cache_name']?>/preview2/<?php print $vars['dest']?>" width="100%" height="500">
+<iframe id="prev_iframe" src="" width="100%" height="500">
   <p>Your browser does not support iframes.</p>
 </iframe>
