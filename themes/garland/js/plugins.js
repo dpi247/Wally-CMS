ba6fdd19dc622d20d2bd00a147d8937029ae43d4
dpi <dpi@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-06-20 13:35:30 +0000

    Julien a fait une erreur de Commit, je l'ai enfin retrouvée ...

--- themes/garland/js/plugins.js
+++
@@ -1,9 +0,0 @@
-(function(jQuery) {
-	jQuery.fn.equalHeights = function(minheight) {
-		var maxheight = minheight || 0;
-		jQuery(this).children().each(function(){
-			maxheight = (jQuery(this).height() > maxheight) ? jQuery(this).height() : maxheight;
-		});
-		jQuery(this).children().css('height', maxheight);
-	}
-})(jQuery);
