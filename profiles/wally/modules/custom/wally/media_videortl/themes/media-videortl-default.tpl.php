3da2b09fb51190f4df325f2c08251a2d447cc3c8
jda <jda@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-07-19 09:05:08 +0000

    Correction import video RTL
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/branches/1.5@62617 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- profiles/wally/modules/custom/wally/media_videortl/themes/media-videortl-default.tpl.php
+++ profiles/wally/modules/custom/wally/media_videortl/themes/media-videortl-default.tpl.php
@@ -25,10 +25,10 @@ foreach ($expl_vid_id as $id_part) {
 ?>
 <div id="media-videortl-default-external-<?php print $video_id; ?>">
 <object id="<?php print $video_id; ?>" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="<?php print $options['width']; ?>" height="<?php print $options['height']; ?>">
-	<param name="movie" value="http://www.rtl.be/videos/GED_vp/00230000/237200/237206.swf?bEmbed=1&bHideInfo=0&VideoID=<?php print $video_id; ?>"></param>
+	<param name="movie" value="http://www.rtl.be/videos/GED_vp/00230000/237200/237206.swf?bEmbed=1&bHideInfo=0&VideoID=<?php print $options['video_id']; ?>"></param>
 	<param name="allowFullScreen" value="<?php print $options['fullscreen']; ?>"></param>
 	<param name="allowscriptaccess" value="always"></param>
-	<embed src="http://www.rtl.be/videos/GED_vp/00230000/237200/237206.swf?bEmbed=1&bHideInfo=0&VideoID=<?php print $video_id; ?>" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="600" height="338"></embed>
+	<embed src="http://www.rtl.be/videos/GED_vp/00230000/237200/237206.swf?bEmbed=1&bHideInfo=0&VideoID=<?php print $options['video_id']; ?>" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="600" height="338"></embed>
 </object>
 </div>
   	
