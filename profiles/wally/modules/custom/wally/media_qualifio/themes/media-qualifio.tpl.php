9a9bbf3aca8cd405d1a3ab60154ab8b76e19e333
jda <jda@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-07-19 12:52:54 +0000

    embed Qualifio
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/branches/1.5@62644 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

---
+++ profiles/wally/modules/custom/wally/media_qualifio/themes/media-qualifio.tpl.php
@@ -0,0 +1,10 @@
+<?php
+// $Id: media-qualifio.tpl.php $
+
+/**
+ * @file media_qualifio/themes/media-qualifio.tpl.php
+ *
+ * Template file for theme('media_qualifio', $media, $variables).
+ */
+?>
+<iframe src="http://player.qualifio.com/08/v1.cfm?id=<?php print $media; ?>&iframe=true" width="<?php print $width; ?>" height="<?php print $height; ?>" scrolling="no" frameborder="0" hspace="0" vspace="0"></iframe>
