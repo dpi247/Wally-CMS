88ac955f95389828afbcbb1e5c4b0edf096f07a4
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-07-19 11:45:36 +0000

    dest delete button
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@62634 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- profiles/wally/modules/custom/wally/cckdestinations/js/cckdestinations.js
+++ profiles/wally/modules/custom/wally/cckdestinations/js/cckdestinations.js
@@ -3,10 +3,13 @@ function updateList() {
 		taxo = this.id;
 		targ = taxo.replace("tid", "target");
 		lay = taxo.replace("tid", "layout");
+		del = taxo.replace("tid", "delete");
 		var selected_targ = $("#"+targ).find("option:selected").attr("value");
 		var selected_lay = $("#"+lay).find("option:selected").attr("value");
 		makeSublist(taxo, targ, true, selected_targ);
 		makeSublist(targ, lay, true, selected_lay);
+		
+		addDeleteAction(del);
 	});
 }
 
@@ -29,6 +32,34 @@ function makeSublist(parent, child, isSubselectOptional, childVal) {
 		$("#"+child).trigger("change");
 		$("#"+child).focus();
 	});
+}
+
+function addDeleteAction(del) {
+	if ($("#"+del).length > 0) {
+		$("#"+del).click(function() {
+			var cur_taxo = this.id.replace("delete", "tid");
+			var selected = $("#"+cur_taxo).find("option:selected");
+			selected.removeAttr("selected");
+			$("#"+cur_taxo+' option[value="0"]').attr("selected", "selected");
+
+			var not_found = true;
+			var parent_tr = $("#"+cur_taxo);
+			while (not_found) {
+				parent_tr = parent_tr.parent();
+				if (parent_tr.hasClass("draggable")) {
+					not_found = false;
+				}
+			}
+			parent_tr.hide();
+			
+			var cur_targ = cur_taxo.replace("tid", "target");
+			var cur_lay = cur_taxo.replace("tid", "layout");
+			var cur_selected_targ = $("#"+cur_targ).find("option:selected").attr("value");
+			var cur_selected_lay = $("#"+cur_lay).find("option:selected").attr("value");
+			makeSublist(cur_taxo, cur_targ, true, cur_selected_targ);
+			makeSublist(cur_targ, cur_lay, true, cur_selected_lay);
+		});
+	}
 }
 
 $(document).ready(function() {
