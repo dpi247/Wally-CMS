95070b66a336906e50999b1158556a5062faa922
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-08-22 09:29:09 +0000

    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/branches/1.5@65170 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- profiles/wally/modules/custom/wally/wallyflowpublish/theme/pane-wallyflowpublish-wallyflowpublish.tpl.php
+++ profiles/wally/modules/custom/wally/wallyflowpublish/theme/pane-wallyflowpublish-wallyflowpublish.tpl.php
@@ -6,13 +6,13 @@
 $view = views_get_view('default_destination_view_feed');
 $display = $view->display['feed_1'];
 $view_path = $display->display_options['path'];
-$expl_path = explode('%', $view_path);
+$expl_path = explode('/%/', $view_path);
 
 $taxos = taxonomy_get_tree(variable_get('wallymport_destinationpath', 0));
 $contenu = "<ul>";
 foreach ($taxos as $taxo){
 	$name = (string)($taxo->name);
-	$link = '/'.$expl_path[0].$name;
+	$link = '/'.$expl_path[0].'/'.$name;
 	$link = (isset($expl_path[1]) && $expl_path[1] != '') ? $link.'/'.$expl_path[1] : $link;
 	$contenu .='<li><a href="'.$link.'">'.$name."</a></li>";
 }
