6522909ce8a8b60ede40ef7c5596fb118a0355f1
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-08-26 13:59:14 +0000

    links correct
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/branches/1.5@65847 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- profiles/wally/modules/custom/wally/wallyflowpublish/theme/pane-wallyflowpublish-wallyflowpublish.tpl.php
+++ profiles/wally/modules/custom/wally/wallyflowpublish/theme/pane-wallyflowpublish-wallyflowpublish.tpl.php
@@ -7,12 +7,14 @@ $view = views_get_view('default_destination_view_feed');
 $display = $view->display['feed_1'];
 $view_path = $display->display_options['path'];
 $expl_path = explode('/%/', $view_path);
+$dest_vid = variable_get('wallymport_destinationpath', 0);
 
-$taxos = taxonomy_get_tree(variable_get('wallymport_destinationpath', 0));
+$taxos = taxonomy_get_tree();
 $contenu = "<ul>";
 foreach ($taxos as $taxo){
 	$name = (string)($taxo->name);
-	$link = '/'.$expl_path[0].'/'.$name;
+	$link_name = wallytoolbox_taxonomy_get_path_by_tid_or_term($taxo->tid, $dest_vid);
+	$link = '/'.$expl_path[0].'/'.$link_name;
 	$link = (isset($expl_path[1]) && $expl_path[1] != '') ? $link.'/'.$expl_path[1] : $link;
 	$contenu .='<li><a href="'.$link.'">'.$name."</a></li>";
 }
