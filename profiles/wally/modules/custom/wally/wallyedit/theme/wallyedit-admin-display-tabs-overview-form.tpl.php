f3d965f2813feae8f583b635c37b7b6763bfdebc
ddu <ddu@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-09-13 10:39:58 +0000

    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@67414 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- profiles/wally/modules/custom/wally/wallyedit/theme/wallyedit-admin-display-tabs-overview-form.tpl.php
+++ profiles/wally/modules/custom/wally/wallyedit/theme/wallyedit-admin-display-tabs-overview-form.tpl.php
@@ -52,19 +52,21 @@
       <tr class="tab-message tab-<?php print $tab['tid']?>-message <?php print empty($tabs_elements[$tab['tid']]) ? 'tab-empty' : 'tab-populated'; ?>">
         <td colspan="6"><em><?php print t('No tabs in this region'); ?></em></td>
       </tr>
-      <?php foreach ($tabs_elements[$tab['tid']] as $delta => $data): ?>
-        
-          <tr class="draggable <?php print $row % 2 == 0 ? 'odd' : 'even'; ?> <?php print $data->row_class ? ' '. $data->row_class : ''; ?>">
-            <td class="block"><?php print $data->label; ?></td>
-            <td><?php print $data->region_select; ?></td>
-            <td><?php print $data->group_select; ?></td>
-            <td><?php print $data->weight_select; ?></td>
-            <td><?php print $data->configure_link; ?></td>
-            <td colspan="6">
-            </td>
-          </tr>
-      <?php $row++; ?>
-      <?php endforeach; ?>
+      <?php if(isset($tabs_elements[$tab['tid']])):?>
+        <?php foreach ($tabs_elements[$tab['tid']] as $delta => $data): ?>
+          
+            <tr class="draggable <?php print $row % 2 == 0 ? 'odd' : 'even'; ?> <?php print $data->row_class ? ' '. $data->row_class : ''; ?>">
+              <td class="block"><?php print $data->label; ?></td>
+              <td><?php print $data->region_select; ?></td>
+              <td><?php print $data->group_select; ?></td>
+              <td><?php print $data->weight_select; ?></td>
+              <td><?php print $data->configure_link; ?></td>
+              <td colspan="6">
+              </td>
+            </tr>
+        <?php $row++; ?>
+        <?php endforeach; ?>
+	<?php endif;?>
     <?php endforeach; ?>
   </tbody>
 </table>
