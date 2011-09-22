b385f6e402584316c237643801a560d7da52c211
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-09-22 12:34:10 +0000

    main story ok
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@68681 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- profiles/wally/modules/custom/wally/wallyedit/theme/wallyedit-node-accordion-form.tpl.php
+++ profiles/wally/modules/custom/wally/wallyedit/theme/wallyedit-node-accordion-form.tpl.php
@@ -1,35 +1,33 @@
 <?php
-$sub_element=$form[0];
-$type_name=$sub_element['type']["#value"];
+  module_load_include("inc",'wallyedit','includes/page_form_display_tabs');
+  $tabs=wyditadmin_get_fields_tree($profile_id, $node_type);
+  $type=wydit_get_infos_type($type_name);
+  $cck_fields = $type['fields'];
 
-module_load_include("inc",'wallyedit','includes/page_form_display_tabs');
-$tabs=wyditadmin_get_fields_tree(wallyedit_get_default_profile(), $type_name);
-
-$type=wydit_get_infos_type($type_name);
-$cck_fields = $type['fields'];
-
-$meta_tab_name="meta_".$profile_id.'_'.$node_type;
-$no_tab_name="no_tab";
+  $meta_tab_name="meta_".$profile_id.'_'.$node_type;
+  $no_tab_name="no_tab";
 ?>
 
-<h2 class="title  title-accordion "><span>Titre Accordeon</span></h2>
-
 <div id="accordion_container">
-  <?php  foreach($tabs as $onglet=>$onglet_content):?>
-    <div id="accordion-tab-<?php print $onglet?>" class="accordion-tab">
-      <h2 class="accordion-tab-title"> Accordion Tab title</h2>
-      <?php  foreach($tabs[$onglet]['elements'] as $group_id=>$group_content):?>
+  <?php  foreach($tabs as $onglet => $onglet_content): ?>
+    <?php if ($onglet != 'no_tab'): ?>
+    <div id="accordion-tab-<?php print $onglet; ?>" class="accordion-tab">
+      <h2 class="accordion-tab-title"><?php print $onglet_content['label']; ?></h2>
+      <?php foreach($tabs[$onglet]['elements'] as $group_id=>$group_content): ?>
         <div class="accordion-group">
-          <h2 class="accordion_group_title"><?php print $tabs[$onglet]['elements'][$group_id]["label"]?></h2>
-          <?php  foreach($tabs[$onglet]['elements'][$group_id]['fields'] as $element_name=>$element_content):?>
-            <?php if(isset($cck_fields[$element_name]['display_settings']['parent']) && !empty($cck_fields[$element_name]['display_settings']['parent'])):?>
-              <?php print drupal_render($form[0][$cck_fields[$element_name]['display_settings']['parent']][$element_name])?>
-            <?php else:?>
-              <?php print drupal_render($form[0][$element_name])?>
-            <?php endif;?>
-          <?php endforeach;?>
+          <?php if ($group_id != 'no_group'): ?>
+          <h2 class="accordion_group_title"><?php print $tabs[$onglet]['elements'][$group_id]["label"]; ?></h2>
+          <?php endif; ?>
+          <?php foreach($tabs[$onglet]['elements'][$group_id]['fields'] as $element_name=>$element_content): ?>
+            <?php if(isset($cck_fields[$element_name]['display_settings']['parent']) && !empty($cck_fields[$element_name]['display_settings']['parent'])): ?>
+              <?php print drupal_render($form[0][$cck_fields[$element_name]['display_settings']['parent']][$element_name]); ?>
+            <?php else: ?>
+              <?php print drupal_render($form[0][$element_name]); ?>
+            <?php endif; ?>
+          <?php endforeach; ?>
         </div>
       <?php endforeach;?>
-    </div>   
+    </div>
+    <?php endif; ?>
   <?php endforeach;?>
 </div>
