98876366498e7479577ba1abcc8351e154071cf3
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-10-17 12:12:37 +0000

    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/branches/1.5@72102 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- misc/drupal.js
+++ misc/drupal.js
@@ -258,8 +258,7 @@ Drupal.ahahError = function(xmlhttp, uri) {
     }
   }
   else {
-	  alert('coucou');
-    var message = Drupal.t("An HTTP error @status occurred. @uri", {'@uri': uri, '@status': xmlhttp.status });
+    var message = Drupal.t("An HTTP error @status occurred. \n@uri", {'@uri': uri, '@status': xmlhttp.status });
   }
   return message.replace(/\n/g, '<br />');
 }
