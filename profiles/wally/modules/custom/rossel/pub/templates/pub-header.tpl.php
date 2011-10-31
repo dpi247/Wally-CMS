d4575890b3f4037da107b3c055d087f88877fa04
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-10-31 09:45:12 +0000

    merge
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@74094 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

---
+++ profiles/wally/modules/custom/rossel/pub/templates/pub-header.tpl.php
@@ -0,0 +1,9 @@
+<?php
+// $Id: pub-header.tpl.php,v 1.0 2011/10/12 HNA $
+
+/**
+ * Available variable:
+ *  - $header_code : contents the code returned by the webservice
+ */
+ 
+drupal_add_js($header_code, 'inline'); ?>
