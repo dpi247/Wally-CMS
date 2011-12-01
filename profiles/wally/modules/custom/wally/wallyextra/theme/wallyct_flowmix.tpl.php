e2dfb6d3ba5620189954ff3ebb9889be1d3692a2
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-12-01 10:31:32 +0000

    debug
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/branches/1.5@77210 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- profiles/wally/modules/custom/wally/wallyextra/theme/wallyct_flowmix.tpl.php
+++ profiles/wally/modules/custom/wally/wallyextra/theme/wallyct_flowmix.tpl.php
@@ -9,7 +9,7 @@ if (!function_exists('rss_feed')) {
     return $content; 
   }
 }
-
+print_r(debug_backtrace());
 if (isset($options['override_title'])) {
   $title = t($options['override_title_text']);
 } else {
