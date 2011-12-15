041c24e4910afe5d86e16fe42b9336d462e78b5c
ddu <ddu@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2011-12-15 09:00:23 +0000

    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/branches/1.5@78799 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- index.php
+++ index.php
@@ -13,16 +13,14 @@
 
 require_once './includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-
+/*
 if(arg(0)=='taxonomy' and arg(1)=='term'){
-
-  $menu_item=menu_get_item($path);
+  $menu_item=menu_get_item();
   if($menu_item['page_callback']!='page_manager_term_view'){
   variable_set('site_offline',TRUE);
   }
-  
-  ;
 }
+*/
 $return = menu_execute_active_handler();
 
 // Menu status constants are integers; page content is a string.
