444e137a683f6815c705850cb1cac1fd868e4d3f
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2012-02-01 08:17:04 +0000

    hack performances

--- index.php
+++ index.php
@@ -14,6 +14,14 @@
 require_once './includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
+if(arg(0) == 'taxonomy' && arg(1) == 'term') {
+  $menu_item = menu_get_item();
+  if($menu_item['page_callback'] != 'page_manager_term_view') {
+    menu_rebuild();
+    //variable_set('site_offline',TRUE);
+  }
+}
+
 $return = menu_execute_active_handler();
 
 // Menu status constants are integers; page content is a string.
