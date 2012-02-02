280560378c09adac9fffd2c51effcf476361ee7d
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2012-02-02 08:45:13 +0000

    esi
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@82623 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

---
+++ profiles/wally/modules/custom/wally/wallyesi/theme/wallyesi_esitag_block.tpl.php
@@ -0,0 +1,3 @@
+<?php dsm($variables);?>
+BLOCK		
+<esi:include srsc="http://example.com/1.html" alt="http://bak.example.com/2.html" onerror="continue" timeout="20" onerror="continue"/>
---
+++ profiles/wally/modules/custom/wally/wallyesi/theme/wallyesi_esitag_ctoolscontenttype.tpl.php
@@ -0,0 +1,9 @@
+<?php 
+dsm($variables);
+$attributes_string='';
+foreach ($variables["esi"]['attributes'] as $key=>$value){
+  
+  $attributes_string.="$key =\"$value\" ";
+}
+?>
+<esi:<?php print$variables['esi']['action']?> <?php print $attributes_string?> />
