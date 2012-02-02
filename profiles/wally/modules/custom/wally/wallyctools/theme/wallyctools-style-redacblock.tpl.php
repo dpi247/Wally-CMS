2b4ce5c873d27728b2316dca97850c55c684b450
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2012-02-02 08:41:58 +0000

    spliting redacblock
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/branches/1.5@82619 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- profiles/wally/modules/custom/wally/wallyctools/theme/wallyctools-style-redacblock.tpl.php
+++ profiles/wally/modules/custom/wally/wallyctools/theme/wallyctools-style-redacblock.tpl.php
@@ -2,13 +2,13 @@
 /**
  * @file
  * Template to display destinations views.
- *
+ * 
  * @see template_preprocess_wallyctools_style_redacblock.
  *
  * $view: The view.
  * $destination: the destination argument.
  * $target: The targeted block.
- *
+ * 
  */
 foreach($rows as $row){
   print($row);
