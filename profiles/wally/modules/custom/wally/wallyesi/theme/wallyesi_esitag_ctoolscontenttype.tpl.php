368b024a2d404a238014a1bb67b51f52f6b3e920
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2012-03-23 09:11:17 +0000

    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@88785 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- profiles/wally/modules/custom/wally/wallyesi/theme/wallyesi_esitag_ctoolscontenttype.tpl.php
+++ profiles/wally/modules/custom/wally/wallyesi/theme/wallyesi_esitag_ctoolscontenttype.tpl.php
@@ -2,7 +2,7 @@
 $attributes_string='';
 foreach ($variables["esi"]['attributes'] as $key=>$value){
   
-  $attributes_string.="$key =\"$value\" ";
+  $attributes_string.="$key=\"$value\" ";
 }
 ?>
 <esi:<?php print$variables['esi']['action']?> <?php print $attributes_string?> />
