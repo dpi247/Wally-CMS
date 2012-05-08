ec59ce9828cb92277342d28cf50a2a241509621d
ddu <ddu@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2012-05-08 19:01:44 +0000

    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/branches/1.5@93488 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- sites/all/themes/wallydemo/template.php
+++ sites/all/themes/wallydemo/template.php
@@ -252,7 +252,8 @@ function wallydemo_preprocess_node_build_embedded_documents(&$vars){
           else{
             dsm(wallydemo_get_digitalobject_infos_and_display($embed));
             dsm($embed);
-            $width=$height='auto';
+            $width=$height='100%';
+            $height="4OOpx";
             $url=url($embed->field_objectfile[0]["filepath"],array('absolute'=>TRUE));
             $content='<iframe src="http://docs.google.com/viewer?url='.$url.'&embedded=true" width="'.$width.'" height="'.$height.'" style="border: none;"></iframe>';
             $title=$node->embed_videos[$embed->nid]['title'];
