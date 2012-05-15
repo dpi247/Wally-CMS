a2e725ec9f489434973ffa37a730b8cf2c730487
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2012-05-15 14:28:37 +0000

    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/branches/1.5@94373 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- sites/all/themes/wallydemo/template.php
+++ sites/all/themes/wallydemo/template.php
@@ -428,12 +428,10 @@ function theme_wallydemo_article_links_lists($linkslist){
 }
 
 function wallydemo_preprocess_node(&$vars) {
+  $node=$vars['node'];
+  $vars = array('node' => $node);
   $node=&$vars['node'];
-  static $coucou = TRUE;
-  if ($coucou) {
-    $coucou = FALSE; 
-  dsm($vars);
-}
+
   if($node->type=="wally_articlepackage" ||$node->type=="wally_pollpackage" || $node->type=="wally_gallerypackage"){
 
     $pub_date = $node->field_publicationdate[0];
