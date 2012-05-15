84ee507afb6d2d1ebdf2345111dad900fa081e2c
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2012-05-15 12:19:36 +0000

    check module exist
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/branches/1.5@94311 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- sites/all/themes/wallydemo/templates/sp_recherche_menu.tpl.php
+++ sites/all/themes/wallydemo/templates/sp_recherche_menu.tpl.php
@@ -1,7 +1,9 @@
 <li class="services recherche_menu">
   <?php 
-    variable_set('apachesolr_panels_search_page', variable_get('apachesolr_path', '/solr'));
-    //print str_replace('Search','OK',drupal_get_form("apachesolr_panels_search_block"));
-    print drupal_get_form("apachesolr_panels_search_block");
+    if (module_exists('apachesolr_panels')) {
+      variable_set('apachesolr_panels_search_page', variable_get('apachesolr_path', '/solr'));
+      //print str_replace('Search','OK',drupal_get_form("apachesolr_panels_search_block"));
+      print drupal_get_form("apachesolr_panels_search_block");
+    }
   ?>
 </li> 
