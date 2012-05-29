4d39b83ce76a6207ae023b1bb5fec801160ed095
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2012-05-29 14:33:50 +0000

    refactorisation, memcached et stats follower
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@95407 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- profiles/wally/modules/custom/wally/wallystat/templates/wallystat-show-stats-48h.tpl.php
+++ profiles/wally/modules/custom/wally/wallystat/templates/wallystat-show-stats-48h.tpl.php
@@ -1,24 +1,19 @@
 <?php
 
-module_load_include('module','chart');
-$href_value = $_GET['hrefvalue'];
-$node_path = drupal_get_normal_path(trim($href_value, '/'));
-$expl_path = explode('/', $node_path);
-
-if ($expl_path[0] == 'node' && is_numeric($expl_path[1])) {
-  $nid = $expl_path[1];
-  
+if ($nid) {
   $temp_node_hours_stats = array();
   $node_hours_stats_db = db_query('SELECT timestamp, SUM(count) FROM {wallystat_node_hourly_counter} WHERE nid = "%s" GROUP BY timestamp ORDER BY timestamp ASC', $nid);
-  while ($stat = db_fetch_array($node_hours_stats_db))
+  while ($stat = db_fetch_array($node_hours_stats_db)) {
     $temp_node_hours_stats[$stat['timestamp']] = $stat['SUM(count)'];
+  }
   
   $current_tstamp = time() - date('i')*60 - date('s');
   $last_tstamp = $current_tstamp - 48*60*60;
   
   $node_hours_stats = array();
-  for ($tstamp = $last_tstamp; $tstamp < $current_tstamp; $tstamp += 60*60)
+  for ($tstamp = $last_tstamp; $tstamp < $current_tstamp; $tstamp += 60*60) {
     $node_hours_stats[$tstamp] = isset($temp_node_hours_stats[$tstamp]) ? $temp_node_hours_stats[$tstamp] : '0';
+  }
   
   $chart = array(
     '#chart_id' => 'node_views_48h',
@@ -29,28 +24,22 @@ if ($expl_path[0] == 'node' && is_numeric($expl_path[1])) {
     '#adjust_resolution' => TRUE,
     '#mixed_axis_labels' => array(
       CHART_AXIS_Y_LEFT => array(
-        array(
-          chart_mixed_axis_range_label(0, max($node_hours_stats)),
-        ),
-        array(
-          chart_mixed_axis_label(t('Views'), 90),
-        ),
+        array(chart_mixed_axis_range_label(0, max($node_hours_stats))),
+        array(chart_mixed_axis_label(t('Views'), 90)),
       ),
       CHART_AXIS_X_BOTTOM => array(
-        array(
-          chart_mixed_axis_range_label(-48, 0),
-        ),
+        array(chart_mixed_axis_range_label(-48, 0)),
       ),
     ),
     '#grid_lines' => chart_grid_lines(10, 10, 1, 5),
   );
   
-  if($display_title==FALSE){
+  if ($display_title == FALSE) {
     unset($chart['#title']);
-    $chart['#mixed_axis_labels'][CHART_AXIS_Y_LEFT][0]=array();
-    $chart['#mixed_axis_labels'][CHART_AXIS__X_BOTTOM][0]=array();
+    $chart['#mixed_axis_labels'][CHART_AXIS_Y_LEFT][0] = array();
+    $chart['#mixed_axis_labels'][CHART_AXIS_X_BOTTOM][0] = array();
   }
-  
+
   $rendered_chart = chart_render($chart);
   
   print $rendered_chart ? $rendered_chart : t('Chart error.');
--- profiles/wally/modules/custom/wally/wallystat/updater.php
+++ profiles/wally/modules/custom/wally/wallystat/updater.php
@@ -1,53 +1,82 @@
 <?php
-if (isset($_GET['nid']) && is_numeric($_GET['nid'])){
 
-  $nid = $_GET['nid'];
-  $tstamp = time();	
-	
+if (isset($_GET['nid']) && is_numeric($_GET['nid'])) {
+  
+  function wallystat_cache_entry($cid, $datas) {
+    $cache_inc = variable_get('cache_inc', './includes/cache.inc');
+
+    if (true||strstr($cache_inc, '/memcache.inc')) {
+      $cached_lock = cache_get('wallystat_lock');
+      if ($cached_lock && isset($cached_lock->data) && !empty($cached_lock->data) && is_numeric($cached_lock->data)) {
+        $lock = $cached_lock->data;
+      } else {
+        $lock = 1;
+      }
+
+      $cached_entries_index = cache_get('wallystat_entries_index_'.$lock);
+      if ($cached_entries_index && isset($cached_entries_index->data) && !empty($cached_entries_index->data)) {
+        $entries_index = $cached_entries_index->data;
+      } else {
+        $entries_index = '';
+      }
+      $entries_index .= $cid.'--';
+      cache_set('wallystat_entries_index_'.$lock, $entries_index);
+    }
+
+    cache_set($cid, $datas, 'cache_wallystat_tempdata');
+  }
+
+  function wallystat_updater_shutdown() {
+    if ($last_error = error_get_last()) {
+      if (function_exists('cache_set') && $last_error['type'] <= E_PARSE) {
+        wallystat_cache_entry(uniqid('error_'), $last_error);
+      }
+    }
+  }
+
+  register_shutdown_function('wallystat_updater_shutdown');
+
   //Ceci n'est pas encore utilisé dans la v1... le but est de créer les trending topics.
   if (isset($_GET['terms'])){
-	//ON s'assure que tous les termes ici sont bien des id numériques et on recrée $safe_terms avec les valeurs vérifiées.
-	$term_array = explode(' ', $_GET['terms']);
-	if (is_array($term_array)){
-	  $safe_terms = array();
-	  foreach ($term_array as $term){
-		if (is_numeric($term)){
-		  $safe_terms[] = $term; 
-		}
-	  }
-	  $terms = implode(' ', $safe_terms);
-	}
+    //ON s'assure que tous les termes ici sont bien des id numériques et on recrée $safe_terms avec les valeurs vérifiées.
+    $term_array = explode(' ', $_GET['terms']);
+    if (is_array($term_array)) {
+      $safe_terms = array();
+      foreach ($term_array as $term) {
+        if (is_numeric($term)) {
+          $safe_terms[] = $term;
+        }
+      }
+      $terms = implode(' ', $safe_terms);
+    }
   }
-  
+
   $wally_install_root = $_SERVER['DOCUMENT_ROOT'];
-	
   chdir($wally_install_root);
   include_once ('./includes/bootstrap.inc');
-  drupal_bootstrap(DRUPAL_BOOTSTRAP_DATABASE);
-
-  $params = array();
-  $params['nid'] = $nid;
-  $params['timestamp']  = $tstamp;
-  
-  if (isset($terms) && $terms != ''){
-    $params['title']   = $_GET['title'];
-    $params['param01'] = $_GET['param01'];
-    $params['param02'] = $_GET['param02'];
-    $params['param03'] = $_GET['param03'];
-    $params['param04'] = $_GET['param04'];
-    $params['param05'] = $_GET['param05'];
-    $params['param06'] = $_GET['param06'];
-    $params['param07'] = $_GET['param07'];
-    $params['param08'] = $_GET['param08'];
-    $params['param09'] = $_GET['param09'];
-    $params['param10'] = $_GET['param10'];
-    $params['terms']   = $terms;
-    
-    cache_set('wallystat_'.$nid.rand(), $params, 'cache_wallystat_tempdata');  
-   } else {
-    cache_set('wallystat_'.$node.rand(), $params, 'cache_wallystat_tempdata');
+  drupal_bootstrap(DRUPAL_BOOTSTRAP_CONFIGURATION);
+  $cache_inc = variable_get('cache_inc', './includes/cache.inc');
+  switch (true) { // memcache.inc needs less bootstrap than cache.inc
+    case strstr($cache_inc, '/memcache.inc') :
+      drupal_bootstrap(DRUPAL_BOOTSTRAP_EARLY_PAGE_CACHE);
+      break;
+    case strstr($cache_inc, '/cache.inc') :
+    default :
+      drupal_bootstrap(DRUPAL_BOOTSTRAP_DATABASE);
+      break;
   }
+
+  $datas = array();
+  $datas['nid'] = $_GET['nid'];
+  $datas['title'] = $_GET['title'];
+  for ($i = 1; $i <= 10; $i++) {
+    $id = ($i == 10) ? 'param'.$i : 'param0'.$i;
+    $datas[$id] = $_GET[$id];
+  }
+  if (isset($terms) && $terms != '') {
+    $datas['terms'] = $terms;
+  }
+
+  $cid = uniqid('wallystat_'.$_GET['nid'].'_');
+  wallystat_cache_entry($cid, $datas);
 }
-
-
-
