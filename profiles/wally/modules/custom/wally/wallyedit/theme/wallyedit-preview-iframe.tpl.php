726626af623cf9cea2b1b42e51caf85da355a588
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2012-07-18 14:57:29 +0000

    loading gif pendant le chargement du preview
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/trunk/wally@100239 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- profiles/wally/modules/custom/wally/wallyedit/theme/wallyedit-preview-iframe.tpl.php
+++ profiles/wally/modules/custom/wally/wallyedit/theme/wallyedit-preview-iframe.tpl.php
@@ -1,4 +1,5 @@
 <h1><?php print t('Preview (Will be displayed in all the destination views of the page)'); ?>: </h1>
+<div id='loading_preview'></div>
 <iframe id="prev_iframe" src="" width="100%" height="500">
   <p><?php print t('Your browser does not support iframes.'); ?></p>
 </iframe>
