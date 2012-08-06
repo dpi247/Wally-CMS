ea0dc9f3e3e90b93504a0054578cf72c13319cc7
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2012-08-06 12:30:19 +0000

    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/branches/1.5@101940 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- sites/all/themes/wallydemo/templates/node-wally_pollobject.tpl.php
+++ sites/all/themes/wallydemo/templates/node-wally_pollobject.tpl.php
@@ -1,5 +1,6 @@
 <?php 
 print $node->field_body[0]['value'];
+module_load_include('inc', 'webform', 'includes/webforms.submissions');
 if (!_webform_submission_user_limit_check($node)){
   dsm('cc');
   print $node->content['webform']['#value'];
