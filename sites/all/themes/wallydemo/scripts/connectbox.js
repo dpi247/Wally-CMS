2894ca885833f32926e0f0fa1b7406922374448f
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2012-08-07 13:51:37 +0000

    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/branches/1.5@102119 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- sites/all/themes/wallydemo/scripts/connectbox.js
+++ sites/all/themes/wallydemo/scripts/connectbox.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
 	$('.connect-box-open').click(function() {
-        $('#connect-overlay').fadeIn('fast',function() {
-            $('#connect-box').animate({'top':'160px'}, 500);
-        });
-    });
-    $('#connect-box-close').click(function() {
-        $('#connect-box').animate({'top':'-200px'}, 500, function() {
-            $('#connect-overlay').fadeOut('fast');
-        });
-    });
+		alert('coucou');
+		$('#connect-overlay').fadeIn('fast',function() {
+			$('#connect-box').animate({'top':'160px'}, 500);
+		});
+	});
+	$('#connect-box-close').click(function() {
+		$('#connect-box').animate({'top':'-200px'}, 500, function() {
+			$('#connect-overlay').fadeOut('fast');
+		});
+	});
 });
