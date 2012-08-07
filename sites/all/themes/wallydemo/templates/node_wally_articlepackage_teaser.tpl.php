9d7da6ec4febf262d46de08c058ee1e25cfd7212
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2012-08-07 10:03:07 +0000

    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/branches/1.5@102076 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- sites/all/themes/wallydemo/templates/node_wally_articlepackage_teaser.tpl.php
+++ sites/all/themes/wallydemo/templates/node_wally_articlepackage_teaser.tpl.php
@@ -120,12 +120,9 @@ $chapeau = "";
 if (isset($strapline)){
   $chapeau = "<p class=\"chapeau\">" .$strapline ."</p>";
 }
-dsm($mainstory);
-if (isset($mainstory->field_textchapo[0]['safe']) && !empty($mainstory->field_textchapo[0]['safe'])) {
-  $texte_article = $mainstory->field_textchapo[0]['safe'];
-} else {
-  $texte_article = substr($mainstory->field_textbody[0]['safe'], 0, 500) . '</a> ...</p>';
-}
+
+$texte_article = substr($mainstory->field_textbody[0]['safe'], 0, 500) . '</a> ...</p>';
+
 $signature = "<p class=\"auteur\">".$package_signature."</p>";
 
 drupal_add_css($themeroot . '/css/article.css');
