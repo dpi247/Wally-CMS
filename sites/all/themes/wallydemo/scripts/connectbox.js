5e38fe50822a7539c7ec55b4c4bba2da7cb91fed
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2012-08-07 15:20:52 +0000

    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/branches/1.5@102181 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- sites/all/themes/wallydemo/scripts/connectbox.js
+++ sites/all/themes/wallydemo/scripts/connectbox.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
 	$('#topmenu li.sudinfo').after('<li class="connect-box-open">'+Drupal.settings.connect_content+'</li>');
-
 	$('.connect-box-open').click(function() {
-		$('#connect-overlay').fadeIn('fast',function() {
+		$('#connect-overlay').fadeIn('fast', function() {
 			$('#connect-box').animate({'top':'300px'}, 1000);
 		});
 	});
