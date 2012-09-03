a6f91134ed8f2219b11b5a04b2b19d4811a3e456
odm <odm@c499b83e-5ffb-0310-9a7e-9101f1ad14f4>
2012-09-03 08:37:15 +0000

    nouvelle version preview
    git-svn-id: http://svn.audaxis.com/svn/repos/Wally/branches/1.5@105071 c499b83e-5ffb-0310-9a7e-9101f1ad14f4

--- profiles/wally/modules/custom/wally/wallyedit/theme/wallyedit-preview-iframe.tpl.php
+++ profiles/wally/modules/custom/wally/wallyedit/theme/wallyedit-preview-iframe.tpl.php
@@ -1,5 +1,14 @@
-<h1><?php print t('Preview (Will be displayed in all the destination views of the page)'); ?>: </h1>
-<div id='loading_preview'></div>
-<iframe id="prev_iframe" src="" width="100%" height="100%">
-  <p><?php print t('Your browser does not support iframes.'); ?></p>
-</iframe>
+<div id="prev_iframe_container">
+  <div id="loading_preview" style="background-color:#000" >
+   
+   <table id="wrapper" style="margin:0;padding:0;border:0; width:100%;height:500px;">
+      <tr>
+         <td style="vertical-align: middle; text-align:center;"><img src="/<?php print  drupal_get_path('module','wallyedit')?>/images/ajax-loader.gif" alt="loading..."/></td>
+      </tr>
+   </table>
+  
+  </div>
+  <iframe id="prev_iframe" src="" width="100%" height="500">
+    <p><?php print t('Your browser does not support iframes.'); ?></p>
+  </iframe>
+</div>
