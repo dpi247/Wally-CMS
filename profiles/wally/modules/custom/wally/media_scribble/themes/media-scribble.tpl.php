dee5f767ba27e40654b970ab421db3ed4a64f9e4
ddu <ddu@audaxis.com>
2013-01-29 10:54:34 +0100

    [Media] WALLY-807 Tink Scribble:déployer le plugin déjà déployé en 2.x sur la branche 1.5.x

---
+++ profiles/wally/modules/custom/wally/media_scribble/themes/media-scribble.tpl.php
@@ -0,0 +1,12 @@
+<?php
+// $Id: media-coveritlive.tpl.php $
+
+/**
+ * @file media_coveritlive/themes/media-coveritlive.tpl.php
+ *
+ * Template file for theme('media_coveritlive', $media, $variables).
+ */
+?>
+
+<iframe src='http://embed.scribblelive.com/Embed/v5.aspx?Id=<?php print $media?>&ThemeId=7346' width='<?php print $width; ?>' height='<?php print $height; ?>' frameborder='0' style='border: 1px solid #000'></iframe>
+
