4defce136568c4cc4f30324e7e05c952abd2dbf6
odm <odm@audaxis.com>
2013-03-01 11:51:42 +0100

    [InlineObjects] WALLY-954 Dans l'Inline embedded object prendre en compte l'alt

--- profiles/wally/modules/custom/cci/cciinlineobjects/theme/filter-inline-objects.tpl.php
+++ profiles/wally/modules/custom/cci/cciinlineobjects/theme/filter-inline-objects.tpl.php
@@ -3,29 +3,8 @@
  * Available vars :
  *  - $embed_object : Drupal node of the inline object to process
  *  - $attributes : array of attributes found in the inline tag
+ *  - $rendered_object : the rendered object
  */
 
-switch ($embed_object->type) {
-  case 'wally_photoobject' :
-    $dimensions = '';
-    if (isset($attributes['preset']) && !empty($attributes['preset'])) {
-      $src = imagecache_create_url($attributes['preset'], $embed_object->field_photofile[0]['filepath']);
-    } elseif ((isset($attributes['width']) && !empty($attributes['width'])) ||
-        (isset($attributes['height']) && !empty($attributes['height']))) {
-      $src = $embed_object->field_photofile[0]['filepath'];
-      $dimensions .= $attributes['width'] ? 'width="'.$attributes['width'].'"' : '';
-      $dimensions .= $attributes['height'] ? 'height="'.$attributes['height'].'"' : '';
-    }
-    if (!empty($src)) {
-      $src = "src=\"$src\"";
-      $align = $attributes['align'] ? 'align="'.$attributes['align'].'"' : '';
-      $runaround = $attributes['runaround'] ? 'style="margin:'.$attributes['runaround'].'px;"' : '';
-      $attributes = $dimensions.$align.$runaround;
-      print "<img $src$attributes />";
-    }
-    break;
-    
-  default :
-    break;
-}
+print $rendered_object;
 ?>
