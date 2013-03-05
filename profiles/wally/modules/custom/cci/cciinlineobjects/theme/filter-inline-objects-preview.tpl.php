8eeecf48a2141bbdf3eed965bc10e997c4179847
odm <odm@audaxis.com>
2013-03-05 15:35:08 +0100

    [InlineObjects] WALLY-323 Gestion des tags <inline> de CCI

---
+++ profiles/wally/modules/custom/cci/cciinlineobjects/theme/filter-inline-objects-preview.tpl.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Available vars :
+ *  - $embed_object : Drupal node of the inline object to process
+ *  - $attributes : array of attributes found in the inline tag
+ *  - $rendered_object : the rendered object
+ */
+
+$rendered_object = '';
+
+switch ($embed_object->type) {
+  case 'wally_photoobject' :
+    $rendered_object = _cciinlineobjects_preprocess_filter_render_photo($embed_object, $attributes);
+    break;
+
+  case 'wally_videoobject':
+    $rendered_object = _cciinlineobjects_preprocess_filter_render_preview($embed_object, $attributes);
+    break;
+
+  case 'wally_linktype':
+    $rendered_object = _cciinlineobjects_preprocess_filter_render_preview($embed_object, $attributes);
+    break;
+
+  default :
+    break;
+}
+
+print $rendered_object;
+?>
