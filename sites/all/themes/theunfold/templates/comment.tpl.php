502125f7c478be68d9df3b61bde275177777aa3c
asl <asl@audaxis.com>
2013-04-24 17:57:08 +0200

    [The Unfold] Wally-1072 Intégrer les commentaires dans le theme the unfold

---
+++ sites/all/themes/theunfold/templates/comment.tpl.php
@@ -0,0 +1,18 @@
+<?php dsm($comment);?>
+<?php print theme('imagecache', '70x70', $comment->picture,'', '');?>
+<div class="comments-body">	
+	<div class="meta">
+		<span class="author"><?php print t('By')?> <?php print $author?></span>
+		<time datetime="<?php print date('Y-m-d', $comment->timestamp);?>">
+		  <?php print t(date('F', $comment->timestamp)).' '.date('j', $comment->timestamp).', '.date('Y', $comment->timestamp);?>
+		</time>
+	</div>
+	<?php print $content;?>
+	<div class="actions">
+		<ul class="no-list">
+			<li><a title="" href="/comment/reply/<?php print $node->nid.'/'.$comment->cid;?>"><i class="lsf">comment</i><?php print t('Reply');?></a></li>
+			<li><a title="" href="/comment/edit/<?php print $comment->cid;?>"><span class="lsf">edit</span><?php print t('Edit');?></a></li>
+			<li><a title="" href="/comment/delete/<?php print $comment->cid;?>"><span class="lsf">delete</span><?php print t('Delete');?></a></li>
+		</ul>
+	</div>
+</div>
