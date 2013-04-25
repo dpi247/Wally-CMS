3b68f309343b71e14957a142544b5c2bc246ceb9
odm <odm@audaxis.com>
2013-04-25 17:06:23 +0200

    WALLY-1077 video rtl (hot fix)

--- profiles/wally/modules/custom/wally/media_videortl/themes/media-videortl-default.tpl.php
+++ profiles/wally/modules/custom/wally/media_videortl/themes/media-videortl-default.tpl.php
@@ -1,6 +1,6 @@
 <?php 
 	$video_id = $variables["variables"]["video_id"]; 
 	$key = $variables["variables"]["key"];
+	$domain = $variables["variables"]["domain"];
 ?>
-<iframe frameborder="0" width="600" height="338" src="http://www.rtl.be/videos/page/rtl-video-en-embed/640.aspx?VideoID=<?php print $video_id ?>&key=<?php print $key ?>"></iframe>
-  	
+<iframe frameborder="0" width="600" height="338" src="http://www.rtl.be/<?php print $domain ?>/page/rtl-video-en-embed/640.aspx?VideoID=<?php print $video_id ?>&key=<?php print $key ?>"></iframe>
