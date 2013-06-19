0c6daf29826f1bc63e8774d0f496e5ca138fe826
asl <asl@audaxis.com>
2013-06-19 11:07:22 +0200

    [The Unfold] Wally-1125 Modification article big

--- sites/all/themes/theunfold/templates/widgets/widget_article_summary.tpl.php
+++ sites/all/themes/theunfold/templates/widgets/widget_article_summary.tpl.php
@@ -1,22 +1,23 @@
 <a href="<?php print $widget['url']; ?>">
-  <?php print $widget['figure'];?>
-  <span class="cat"><?php print $widget['barette']; ?></span>
-  <h2>
+  <h2 class="big-title">
 	<?php print $widget['title'];?>
   </h2>
+  <span class="cat"><?php print $widget['barette']; ?></span>
   <span class="meta">
-	  <?php if ($widget['by'] != NULL){?>
-  	    <span class="author"><?php print t('Published') ?> <time datetime="<?php print $widget['publicationdate'] ?>">
-  		  <?php print date('d/m/Y', $widget['publicationdate']).' '.t('at').' '.date('H:i', $widget['publicationdate'])?></time> <?php print $widget['by']; ?>
-  		</span>
-      <?php } else { ?>
-  	    <span class="author"><?php print t('Published') ?> <time datetime="<?php print $widget['publicationdate'] ?>">
-  		  <?php print date('d/m/Y', $widget['publicationdate']).' '.t('at').' '.date('H:i', $widget['publicationdate'])?></time>
-  		</span>
-      <?php } ?>
-      <span id="comment-count"><i class="icon-comment"></i><?php print $widget['comment_count']; ?>
+	<?php if ($widget['by'] != NULL){?>
+  	  <span class="author"><?php print t('Published') ?> <time datetime="<?php print $widget['publicationdate'] ?>">
+  	    <?php print date('d/m/Y', $widget['publicationdate']).' '.t('at').' '.date('H:i', $widget['publicationdate'])?></time> <?php print $widget['by']; ?>
+  	  </span>
+    <?php } else { ?>
+  	  <span class="author"><?php print t('Published') ?> <time datetime="<?php print $widget['publicationdate'] ?>">
+  	    <?php print date('d/m/Y', $widget['publicationdate']).' '.t('at').' '.date('H:i', $widget['publicationdate'])?></time>
+  	  </span>
+    <?php } ?>
+    <span id="comment-count"><i class="icon-comment"></i><?php print $widget['comment_count']; ?>
       <?php if ($widget['mediaicons']!="") { print "| ".$widget['mediaicons']; } ?>
     </span>
+  </span>
+  <?php print $widget['figure'];?>
 </a>
 <div class="description">
   <p><?php  print $widget['textchapo']; ?></p>
