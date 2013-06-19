c58a49e5ad0abbd28e979cb5b506d967a3e76529
asl <asl@audaxis.com>
2013-06-19 09:58:14 +0200

    [The Unfold] Wally-1125 Corrections html/css

--- sites/all/themes/theunfold/templates/widgets/widget_article_summary_titleonly.tpl.php
+++ sites/all/themes/theunfold/templates/widgets/widget_article_summary_titleonly.tpl.php
@@ -1,7 +1,8 @@
 <a href="<?php print $widget['url']; ?>">
   <h2>
 	<?php print $widget['title'];?>
-	<span class="meta">
+  </h2>
+  <span class="meta">
 	  <?php if ($widget['by'] != NULL){?>
   	    <span class="author">
   	      <?php print t('Published') ?> <time datetime="<?php print $widget['publicationdate'] ?>">
@@ -13,5 +14,4 @@
   	    </span>
       <?php } ?>
     </span>
-  </h2>
 </a>  
